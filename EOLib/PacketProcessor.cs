45bd974d0a3f256585bed2d7e068f064390b6ba0
Ethan Moffat <ethanmoffat@hotmail.com>
2014-10-24 18:43:30 -0700

    Packet send bug
    The encoding function in PacketProcessor had a bug where small packets
    (ie, only one extra byte, like for Face) were not being encoded
    properly, resulting in invalid data server-side. Also ran through
    ReSharper suggestions for the file.

--- EOLib/PacketProcessor.cs
+++ EOLib/PacketProcessor.cs
@@ -1,12 +1,11 @@
 ﻿/* Most of this file was taken straight from EOSERV source code with modifications to convert it to C# */
+/* Some of this file was reverse engineered from EOHax C# Dlls using a decompiler */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace EOLib
 {
+// ReSharper disable UnusedMember.Global
 	public enum PacketFamily : byte
 	{
 		Connection = (byte)1,
@@ -90,7 +89,6 @@ namespace EOLib
 		Net3 = (byte)242,
 		Init = (byte)255,
 	}
-
 	public class PacketProcessor
 	{
 		public static ushort PID(PacketFamily family, PacketAction action)
@@ -100,10 +98,10 @@ namespace EOLib
 
 		public static byte[] EPID(ushort pid)
 		{
-			return new byte[] { (byte)(pid >> 8), (byte)(pid & 0xFF) };
+			return new[] {(byte) (pid >> 8), (byte) (pid & 0xFF)};
 		}
-		
-		public static void Interleave(ref byte[] b)
+// ReSharper restore UnusedMember.Global
+		protected static void Interleave(ref byte[] b)
 		{
 			byte[] numArray = new byte[b.Length];
 			int index1 = 0;
@@ -121,10 +119,10 @@ namespace EOLib
 				numArray[index2] = b[num++];
 				index2 -= 2;
 			}
-			numArray.CopyTo((Array)b, 0);
+			numArray.CopyTo(b, 0);
 		}
 
-		public static void Deinterleave(ref byte[] b)
+		protected static void Deinterleave(ref byte[] b)
 		{
 			byte[] numArray = new byte[b.Length];
 			int index1 = 0;
@@ -142,23 +140,23 @@ namespace EOLib
 				numArray[num++] = b[index2];
 				index2 -= 2;
 			}
-			numArray.CopyTo((Array)b, 0);
+			numArray.CopyTo(b, 0);
 		}
 
-		public static void FlipMSB(ref byte[] b)
+		protected static void FlipMSB(ref byte[] b)
 		{
 			for (int index = 0; index < b.Length; ++index)
-				b[index] = (byte)((uint)b[index] ^ 128U);
+				b[index] = (byte)(b[index] ^ 128U);
 		}
 
-		public static void SwapMultiples(ref byte[] b, int multi)
+		protected static void SwapMultiples(ref byte[] b, int multi)
 		{
 			int num1 = 0;
 			if (multi <= 0)
 				return;
 			for (int index1 = 0; index1 <= b.Length; ++index1)
 			{
-				if (index1 != b.Length && (int)b[index1] % multi == 0)
+				if (index1 != b.Length && b[index1] % multi == 0)
 				{
 					++num1;
 				}
@@ -180,38 +178,38 @@ namespace EOLib
 
 		//instance things
 
-		public byte RecvMulti { get; protected set; }
-		public byte SendMulti { get; protected set; }
+		public byte RecvMulti { get; private set; }
+		public byte SendMulti { get; private set; }
 
-		public PacketProcessor()
+		protected PacketProcessor()
 		{
 			RecvMulti = SendMulti = 0;
 		}
 
 		public void SetMulti(byte recvMulti, byte sendMulti)
 		{
-			if ((int)this.RecvMulti != 0 || (int)this.SendMulti != 0)
+			if (RecvMulti != 0 || SendMulti != 0)
 				throw new ApplicationException("PacketProcessor multiples already set");
 
-			this.RecvMulti = recvMulti;
-			this.SendMulti = sendMulti;
+			RecvMulti = recvMulti;
+			SendMulti = sendMulti;
 		}
 	}
 
 	public class ClientPacketProcessor : PacketProcessor
 	{
-		public int SequenceStart { get; set; }
+		public int SequenceStart { private get; set; }
 
-		public int SequenceValue { get; set; }
+		private int SequenceValue { get; set; }
 
-		public int GetSequence()
+		private int GetSequence()
 		{
 			SequenceValue = (SequenceValue + 1) % 10;
 			int ret = SequenceStart + SequenceValue;
 			return ret;
 		}
 
-		public void AddSequenceByte(ref byte[] original)
+		private void AddSequenceByte(ref byte[] original)
 		{
 			int seq = GetSequence();
 			int extra = (SequenceStart >= 253) ? 2 : 1;
@@ -219,9 +217,9 @@ namespace EOLib
 			byte[] newArray = new byte[original.Length + extra];
 
 			Array.Copy(original, 0, newArray, 0, 2); //family/action copied to [0][1]
-			Array.Copy(Packet.EncodeNumber(seq, extra), 0, newArray, 2, extra); //sequence number copied to [2]([3])
-			if(original.Length >= 2 + extra) //if there is data left to be copied...
-				Array.Copy(original, 2, newArray, 2 + extra, original.Length - 2); //rest of data copied to ([3]|[4])[...]
+			Array.Copy(Packet.EncodeNumber(seq, extra), 0, newArray, 2, extra); //sequence number copied to [2](and [3] if it's a larger number)
+			if(original.Length > 2) //if there is data left to be copied...
+				Array.Copy(original, 2, newArray, 2 + extra, original.Length - 2); //rest of data copied to ([3] or [4] onward)[...]
 
 			original = newArray;
 		}
@@ -231,10 +229,10 @@ namespace EOLib
 			if (SendMulti == 0 || (original.Length > 2 && (PacketFamily)original[2] == PacketFamily.Init && (PacketAction)original[3] == PacketAction.Init))
 				return;
 
-			this.AddSequenceByte(ref original);
-			PacketProcessor.SwapMultiples(ref original, (int)this.SendMulti);
-			PacketProcessor.Interleave(ref original);
-			PacketProcessor.FlipMSB(ref original);
+			AddSequenceByte(ref original);
+			SwapMultiples(ref original, SendMulti);
+			Interleave(ref original);
+			FlipMSB(ref original);
 		}
 
 		public void Decode(ref byte[] original)
@@ -242,9 +240,9 @@ namespace EOLib
 			if (RecvMulti == 0 || ((PacketFamily)original[0] == PacketFamily.Init && (PacketAction)original[1] == PacketAction.Init))
 				return;
 
-			PacketProcessor.FlipMSB(ref original);
-			PacketProcessor.Deinterleave(ref original);
-			PacketProcessor.SwapMultiples(ref original, (int)this.RecvMulti);
+			FlipMSB(ref original);
+			Deinterleave(ref original);
+			SwapMultiples(ref original, RecvMulti);
 		}
 	}
 }
