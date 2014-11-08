d7214494598078ca91fd372360853dfdcb160725
Ethan Moffat <ethanmoffat@hotmail.com>
2014-11-08 15:30:32 -0800

    Players_Agree bug
    Players_Agree doesn't always send a warp animation. But it does on
    Map::Enter! >.<

--- EndlessClient/EndlessClient/Handlers/Players.cs
+++ EndlessClient/EndlessClient/Handlers/Players.cs
@@ -14,9 +14,17 @@ namespace EndlessClient.Handlers
 
 			EndlessClient.Character newGuy = new EndlessClient.Character(pkt);
 
-			WarpAnimation anim = (WarpAnimation) pkt.GetChar();
-			if (pkt.GetByte() != 255)
-				return;
+			byte nextByte = pkt.GetByte();
+			WarpAnimation anim = WarpAnimation.None;
+			if (nextByte != 255) //next byte was the warp animation: sent on Map::Enter in eoserv
+			{
+				pkt.Skip(-1);
+				anim = (WarpAnimation) pkt.GetChar();
+				if (pkt.GetByte() != 255) //the 255 still needs to be read...
+					return;
+			}
+			//else... //next byte was a 255. proceed normally.
+
 			if (pkt.GetChar() == 1) //0 for NPC, 1 for player. In eoserv it is never 0.
 				World.Instance.ActiveMapRenderer.AddOtherPlayer(newGuy, anim);
 		}
