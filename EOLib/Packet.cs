4df5a223da5dc7ca34e840158b638c170dc15aae
Ethan Moffat <ethanmoffat@hotmail.com>
2015-04-23 16:32:40 -0700

    ReSharper conformance
    Packet class conforms to ReSharper suggestions - a lot of redundancies in the code removed (this keyword, unnecessary casts, etc)

--- EOLib/Packet.cs
+++ EOLib/Packet.cs
@@ -1,53 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace EOLib
 {
 	public class Packet
 	{
-		public static readonly int[] Max = new int[3] { 253, 64009, 16194277 };
+		private static readonly int[] Max = { 253, 64009, 16194277 };
+		
 		private int readPos = 2;
 		private int writePos = 2;
-		public const byte Break = (byte)255;
+		
 		private List<byte> data;
 
 		public PacketFamily Family
 		{
-			get
-			{
-				return (PacketFamily)this.data[1];
-			}
-			set
-			{
-				this.data[1] = (byte)value;
-			}
+			get { return (PacketFamily) data[1]; }
 		}
 
 		public PacketAction Action
 		{
-			get
-			{
-				return (PacketAction)this.data[0];
-			}
-			set
-			{
-				this.data[0] = (byte)value;
-			}
+			get { return (PacketAction) data[0]; }
 		}
 
 		public int ReadPos
 		{
-			get
-			{
-				return this.readPos;
-			}
+			get { return readPos; }
 			set
 			{
-				if (value < 0 || value > this.Length)
+				if (value < 0 || value > Length)
 					throw new IndexOutOfRangeException("Seek out of range of packet");
-				this.readPos = value;
+				readPos = value;
 			}
 		}
 
@@ -55,13 +38,13 @@ namespace EOLib
 		{
 			get
 			{
-				return this.writePos;
+				return writePos;
 			}
 			set
 			{
-				if (value < 0 || value > this.Length)
+				if (value < 0 || value > Length)
 					throw new IndexOutOfRangeException("Seek out of range of packet");
-				this.writePos = value;
+				writePos = value;
 			}
 		}
 
@@ -69,7 +52,7 @@ namespace EOLib
 		{
 			get
 			{
-				return this.data.Count;
+				return data.Count;
 			}
 		}
 
@@ -77,14 +60,12 @@ namespace EOLib
 
 		public Packet(PacketFamily family, PacketAction action)
 		{
-			this.data = new List<byte>(2);
-			this.data.Add((byte)action);
-			this.data.Add((byte)family);
+			data = new List<byte>(2) {(byte) action, (byte) family};
 		}
 
-		public Packet(byte[] data)
+		public Packet(IEnumerable<byte> data)
 		{
-			this.data = new List<byte>((IEnumerable<byte>)data);
+			this.data = new List<byte>(data);
 		}
 
 		public static byte[] EncodeNumber(int number, int size)
@@ -94,16 +75,16 @@ namespace EOLib
 			{
 				if (index >= numArray.Length)
 				{
-					if (number >= Packet.Max[index - 1])
-						number %= Packet.Max[index - 1];
+					if (number >= Max[index - 1])
+						number %= Max[index - 1];
 				}
-				else if (number >= Packet.Max[index - 1])
+				else if (number >= Max[index - 1])
 				{
-					numArray[index] = (byte)(number / Packet.Max[index - 1] + 1);
-					number %= Packet.Max[index - 1];
+					numArray[index] = (byte)(number / Max[index - 1] + 1);
+					number %= Max[index - 1];
 				}
 				else
-					numArray[index] = (byte)254;
+					numArray[index] = 254;
 			}
 			numArray[0] = (byte)(number + 1);
 			return numArray;
@@ -113,82 +94,86 @@ namespace EOLib
 		{
 			for (int index = 0; index < b.Length; ++index)
 			{
-				if ((int)b[index] == 0 || (int)b[index] == 254)
-					b[index] = (byte)0;
+				if (b[index] == 0 || b[index] == 254)
+					b[index] = 0;
 				else
 					--b[index];
 			}
 			int num = 0;
 			for (int index = b.Length - 1; index >= 1; --index)
-				num += (int)b[index] * Packet.Max[index - 1];
-			return num + (int)b[0];
+				num += b[index] * Max[index - 1];
+			return num + b[0];
 		}
 
+// ReSharper disable once UnusedMember.Global
 		public void Clear()
 		{
-			this.data = new List<byte>((IEnumerable<byte>)new byte[2] { this.data[0], this.data[1] });
-			this.readPos = 2;
-			this.writePos = 2;
+			data = new List<byte>(new[] {data[0], data[1]});
+			readPos = 2;
+			writePos = 2;
 		}
 
+// ReSharper disable once UnusedMember.Global
 		public void SetID(PacketFamily family, PacketAction action)
 		{
-			this.data[0] = (byte)action;
-			this.data[1] = (byte)family;
+			data[0] = (byte)action;
+			data[1] = (byte)family;
 		}
 
 		public void AddByte(byte b)
 		{
-			this.data.Insert(this.writePos, b);
-			++this.writePos;
+			data.Insert(writePos, b);
+			++writePos;
 		}
 
+// ReSharper disable once MemberCanBePrivate.Global
 		public void AddBreak()
 		{
-			this.AddByte(byte.MaxValue);
+			AddByte(byte.MaxValue);
 		}
 
 		public void AddChar(byte c)
 		{
-			this.data.InsertRange(this.writePos, (IEnumerable<byte>)Packet.EncodeNumber((int)c, 1));
-			++this.writePos;
+			data.InsertRange(writePos, EncodeNumber(c, 1));
+			++writePos;
 		}
 
 		public void AddShort(short s)
 		{
-			this.data.InsertRange(this.writePos, (IEnumerable<byte>)Packet.EncodeNumber((int)s, 2));
-			this.writePos += 2;
+			data.InsertRange(writePos, EncodeNumber(s, 2));
+			writePos += 2;
 		}
 
 		public void AddThree(int t)
 		{
-			this.data.InsertRange(this.writePos, (IEnumerable<byte>)Packet.EncodeNumber(t, 3));
-			this.writePos += 3;
+			data.InsertRange(writePos, EncodeNumber(t, 3));
+			writePos += 3;
 		}
 
 		public void AddInt(int i)
 		{
-			this.data.InsertRange(this.writePos, (IEnumerable<byte>)Packet.EncodeNumber(i, 4));
-			this.writePos += 4;
+			data.InsertRange(writePos, EncodeNumber(i, 4));
+			writePos += 4;
 		}
 
 		public void AddBytes(byte[] b)
 		{
-			this.data.InsertRange(this.writePos, (IEnumerable<byte>)b);
-			this.writePos += b.Length;
+			data.InsertRange(writePos, b);
+			writePos += b.Length;
 		}
 
+// ReSharper disable once UnusedMember.Global
 		public void AddBytes(byte[] b, int offset, int count)
 		{
 			byte[] numArray = new byte[count];
-			Array.Copy((Array)b, offset, (Array)numArray, 0, count);
-			this.data.InsertRange(this.writePos, (IEnumerable<byte>)numArray);
-			this.writePos += count;
+			Array.Copy(b, offset, numArray, 0, count);
+			data.InsertRange(writePos, numArray);
+			writePos += count;
 		}
 
 		public void AddString(string s)
 		{
-			this.AddBytes(Encoding.ASCII.GetBytes(s));
+			AddBytes(Encoding.ASCII.GetBytes(s));
 		}
 
 		public void AddBreakString(string s)
@@ -196,128 +181,129 @@ namespace EOLib
 			byte[] bytes = Encoding.ASCII.GetBytes(s);
 			for (int index = 0; index < bytes.Length; ++index)
 			{
-				if ((int)bytes[index] == (int)byte.MaxValue)
-					bytes[index] = (byte)121;
+				if (bytes[index] == byte.MaxValue)
+					bytes[index] = 121;
 			}
-			this.AddBytes(bytes);
-			this.AddBreak();
+			AddBytes(bytes);
+			AddBreak();
 		}
 
 		public byte PeekByte()
 		{
-			return this.data[this.readPos];
+			return data[readPos];
 		}
 
 		public byte PeekChar()
 		{
-			return (byte)Packet.DecodeNumber(this.data.GetRange(this.readPos, 1).ToArray());
+			return (byte)DecodeNumber(data.GetRange(readPos, 1).ToArray());
 		}
 
 		public short PeekShort()
 		{
-			return (short)Packet.DecodeNumber(this.data.GetRange(this.readPos, 2).ToArray());
+			return (short)DecodeNumber(data.GetRange(readPos, 2).ToArray());
 		}
 
 		public int PeekThree()
 		{
-			return Packet.DecodeNumber(this.data.GetRange(this.readPos, 3).ToArray());
+			return DecodeNumber(data.GetRange(readPos, 3).ToArray());
 		}
 
 		public int PeekInt()
 		{
-			return Packet.DecodeNumber(this.data.GetRange(this.readPos, 4).ToArray());
+			return DecodeNumber(data.GetRange(readPos, 4).ToArray());
 		}
 
+// ReSharper disable once MemberCanBePrivate.Global
 		public byte[] PeekBytes(int length)
 		{
-			return this.data.GetRange(this.readPos, length).ToArray();
+			return data.GetRange(readPos, length).ToArray();
 		}
 
 		public string PeekFixedString(int length)
 		{
-			return Encoding.ASCII.GetString(this.PeekBytes(length));
+			return Encoding.ASCII.GetString(PeekBytes(length));
 		}
 
 		public string PeekEndString()
 		{
-			return this.PeekFixedString(this.Length - this.readPos);
+			return PeekFixedString(Length - readPos);
 		}
 
 		public string PeekBreakString()
 		{
-			int index = this.readPos;
-			while (index < this.Length && (int)this.data[index] != (int)byte.MaxValue)
+			int index = readPos;
+			while (index < Length && data[index] != byte.MaxValue)
 				++index;
-			return this.PeekFixedString(index - this.readPos);
+			return PeekFixedString(index - readPos);
 		}
 
 		public byte GetByte()
 		{
-			byte num = this.PeekByte();
-			++this.readPos;
+			byte num = PeekByte();
+			++readPos;
 			return num;
 		}
 
 		public byte GetChar()
 		{
-			byte num = this.PeekChar();
-			++this.readPos;
+			byte num = PeekChar();
+			++readPos;
 			return num;
 		}
 
 		public short GetShort()
 		{
-			short num = this.PeekShort();
-			this.readPos += 2;
+			short num = PeekShort();
+			readPos += 2;
 			return num;
 		}
 
 		public int GetThree()
 		{
-			int num = this.PeekThree();
-			this.readPos += 3;
+			int num = PeekThree();
+			readPos += 3;
 			return num;
 		}
 
 		public int GetInt()
 		{
-			int num = this.PeekInt();
-			this.readPos += 4;
+			int num = PeekInt();
+			readPos += 4;
 			return num;
 		}
 
 		public byte[] GetBytes(int length)
 		{
-			byte[] numArray = this.PeekBytes(length);
-			this.readPos += length;
+			byte[] numArray = PeekBytes(length);
+			readPos += length;
 			return numArray;
 		}
 
 		public string GetFixedString(int length)
 		{
-			return Encoding.ASCII.GetString(this.GetBytes(length));
+			return Encoding.ASCII.GetString(GetBytes(length));
 		}
 
 		public string GetEndString()
 		{
-			return this.GetFixedString(this.Length - this.readPos);
+			return GetFixedString(Length - readPos);
 		}
 
 		public string GetBreakString()
 		{
-			string str = this.PeekBreakString();
-			this.readPos += str.Length + 1;
+			string str = PeekBreakString();
+			readPos += str.Length + 1;
 			return str;
 		}
 
 		public void Skip(int bytes)
 		{
-			this.readPos += bytes;
+			readPos += bytes;
 		}
 
 		public byte[] Get()
 		{
-			return this.data.ToArray();
+			return data.ToArray();
 		}
 	}
 }
