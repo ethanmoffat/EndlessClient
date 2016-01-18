af11a7a70c331be4458028196e71eeeea82ae532
Ethan Moffat <ethanmoffat@hotmail.com>
2016-01-18 15:35:40 -0800

    StatusBar File Split
    Split types of status bars into their own separate files.

---
+++ EndlessClient/EndlessClient/HUD/StatusBars/HPStatusBar.cs
@@ -0,0 +1,34 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System;
+using Microsoft.Xna.Framework;
+
+namespace EndlessClient.HUD.StatusBars
+{
+	public class HPStatusBar : BaseStatusBar
+	{
+		public HPStatusBar()
+		{
+			DrawLocation = new Vector2(100, 0);
+			drawArea = new Rectangle((int)DrawLocation.X, (int)DrawLocation.Y, m_elemSourceRect.Width, m_elemSourceRect.Height);
+		}
+
+		protected override void UpdateLabelText()
+		{
+			m_label.Text = string.Format("{0}/{1}", m_stats.HP, m_stats.MaxHP);
+		}
+
+		protected override void DrawStatusBar()
+		{
+			int srcWidth = 25 + (int)Math.Round((m_stats.HP / (double)m_stats.MaxHP) * 79);
+			Rectangle maskSrc = new Rectangle(m_elemSourceRect.X, m_elemSourceRect.Height, srcWidth, m_elemSourceRect.Height);
+
+			SpriteBatch.Begin();
+			SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), m_elemSourceRect, Color.White);
+			SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), maskSrc, Color.White);
+			SpriteBatch.End();
+		}
+	}
+}
---
+++ EndlessClient/EndlessClient/HUD/StatusBars/SPStatusBar.cs
@@ -0,0 +1,35 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System;
+using Microsoft.Xna.Framework;
+
+namespace EndlessClient.HUD.StatusBars
+{
+	public class SPStatusBar : BaseStatusBar
+	{
+		public SPStatusBar()
+		{
+			DrawLocation = new Vector2(320, 0);
+			drawArea = new Rectangle((int)DrawLocation.X, (int)DrawLocation.Y, m_elemSourceRect.Width, m_elemSourceRect.Height);
+			m_elemSourceRect.Offset(m_elemSourceRect.Width * 2, 0);
+		}
+
+		protected override void UpdateLabelText()
+		{
+			m_label.Text = string.Format("{0}/{1}", m_stats.SP, m_stats.MaxSP);
+		}
+
+		protected override void DrawStatusBar()
+		{
+			int srcWidth = 25 + (int)Math.Round((m_stats.SP / (double)m_stats.MaxSP) * 79);
+			Rectangle maskSrc = new Rectangle(m_elemSourceRect.X, m_elemSourceRect.Height, srcWidth, m_elemSourceRect.Height);
+
+			SpriteBatch.Begin();
+			SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), m_elemSourceRect, Color.White);
+			SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), maskSrc, Color.White);
+			SpriteBatch.End();
+		}
+	}
+}
