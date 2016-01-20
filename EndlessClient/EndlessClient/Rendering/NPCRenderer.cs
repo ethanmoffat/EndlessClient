7adec9883cb789b5ec5e28208119428a59cb2770
Ethan Moffat <ethanmoffat@hotmail.com>
2016-01-19 18:38:23 -0800

    NPCRenderer Refactor
    Removed use of Timers from code. Reorganized functions to be cleaner. Arranged functions into groups by their type (public, overrides, etc.)

--- EndlessClient/EndlessClient/Rendering/NPCRenderer.cs
+++ EndlessClient/EndlessClient/Rendering/NPCRenderer.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Linq;
-using System.Threading;
 using EndlessClient.Dialogs;
 using EOLib;
 using EOLib.IO;
@@ -34,36 +33,26 @@ namespace EndlessClient.Rendering
 		public bool Dying { get; private set; }
 		public bool CompleteDeath { get; private set; }
 
+		//drawing related members
 		private readonly EONPCSpriteSheet _npcSheet;
 		private SpriteBatch _sb;
 		private Rectangle _npcTextureFrameRectangle;
-
-		private Timer _walkTimer, _attackTimer;
-
-		private int offX
-		{
-			get { return NPC.X * 32 - NPC.Y * 32 + adjX; }
-		}
-
-		private int offY
-		{
-			get { return NPC.X * 16 + NPC.Y * 16 + adjY; }
-		}
-
-		//additional offsets for the walking animation. added to offX/offY
-		private int adjX, adjY;
-
-		private readonly bool hasStandFrame1;
+		private bool hasStandFrame1;
 		private int _fadeAwayAlpha;
 
-		private readonly EOChatBubble m_chatBubble;
+		private int DrawOffsetX { get { return NPC.X * 32 - NPC.Y * 32 + walkingAdjustedX; } }
+		private int DrawOffsetY { get { return NPC.X * 16 + NPC.Y * 16 + walkingAdjustedY; } }
+
+		//"child" controls added to this NPC
+		private readonly EOChatBubble _chatBubble;
 		private readonly DamageCounter m_damageCounter;
-		private Texture2D baseFrame;
-
-		private TimeSpan? m_lastAnimUpdateTime;
-
 		private XNALabel _mouseoverName;
-		private MouseState m_prevState, m_currState;
+
+		//update related members
+		private int walkingAdjustedX, walkingAdjustedY;
+		private DateTime _actionStartTime;
+		private DateTime _lastAnimUpdateTime;
+		private MouseState _prevMouseState, _currMouseState;
 
 		public NPCRenderer(NPC npc)
 			: base(EOGame.Instance)
@@ -71,73 +60,35 @@ namespace EndlessClient.Rendering
 			NPC = npc;
 			Dying = false;
 			_fadeAwayAlpha = 255;
+			_actionStartTime = DateTime.Now;
+			_lastAnimUpdateTime = DateTime.Now;
 
-			bool success = true;
 			_npcSheet = new EONPCSpriteSheet(((EOGame)Game).GFXManager, this);
-			int tries = 0;
-			do
-			{
-				try
-				{
-					//attempt to get standing frame 1. It will have non-black pixels if it exists.
-					Frame = NPCFrame.StandingFrame1;
-					Texture2D tmp = _npcSheet.GetNPCTexture();
-					Color[] tmpData = new Color[tmp.Width*tmp.Height];
-					tmp.GetData(tmpData);
-					hasStandFrame1 = tmpData.Any(_c => _c.R != 0 || _c.G != 0 || _c.B != 0);
 
-					//get the first non-transparent pixel to determine offsets for name labels and damage counters
-					Frame = NPCFrame.Standing;
-					tmp = _npcSheet.GetNPCTexture();
-					tmpData = new Color[tmp.Width*tmp.Height];
-					tmp.GetData(tmpData);
-					int i = 0;
-					while (i < tmpData.Length && tmpData[i].A == 0) i++;
-					TopPixel = i == tmpData.Length - 1 ? 0 : i/tmp.Height;
-
-				} //this block throws errors sometimes..no idea why. Keep looping until it works.
-				catch (InvalidOperationException)
-				{
-					success = false;
-					tries++;
-				}
-			} while (!success && tries < 3);
-
-			if(tries >= 3)
-				throw new InvalidOperationException("Something weird happened initializing this NPC.");
-
-			m_chatBubble = new EOChatBubble(this);
+			_chatBubble = new EOChatBubble(this);
 			m_damageCounter = new DamageCounter(this);
-			_mouseoverName = new XNALabel(new Rectangle(1, 1, 1, 1), Constants.FontSize08pt75)
-			{
-				Visible = false,
-				Text = NPC.Data.Name,
-				ForeColor = Color.White,
-				AutoSize = false,
-				DrawOrder = (int) ControlDrawLayer.BaseLayer + 3
-			};
-			_mouseoverName.DrawLocation = new Vector2(
-				DrawArea.X + (DrawArea.Width - _mouseoverName.ActualWidth)/2f,
-				DrawArea.Y + TopPixel - _mouseoverName.ActualHeight - 4);
-			_mouseoverName.ResizeBasedOnText();
+			CreateMouseoverName();
 		}
+
+		#region Game Component Overrides
 
 		public override void Initialize()
 		{
-			base.Initialize();
+			base.Initialize(); //required to use the GraphicsDevice property
+
+			InitializeTopPixel();
+			InitializeStandingFrame1();
+
 			_sb = new SpriteBatch(GraphicsDevice);
 
 			Frame = NPCFrame.Standing;
 
-			baseFrame = _npcSheet.GetNPCTexture();
+			var baseFrame = _npcSheet.GetNPCTexture();
 			_npcTextureFrameRectangle = new Rectangle(0, 0, baseFrame.Width, baseFrame.Height);
 			UpdateDrawArea();
 
-			_walkTimer = new Timer(_walkCallback, null, Timeout.Infinite, Timeout.Infinite);
-			_attackTimer = new Timer(_attackCallback, null, Timeout.Infinite, Timeout.Infinite);
-
-			m_chatBubble.Initialize();
-			m_chatBubble.LoadContent();
+			_chatBubble.Initialize();
+			_chatBubble.LoadContent();
 		}
 
 		public override void Update(GameTime gameTime)
@@ -148,93 +99,16 @@ namespace EndlessClient.Rendering
 
 			if (!Game.IsActive) return;
 
-			AnimateNPC(gameTime);
+			UpdateStandingFrameIfNeeded();
+			UpdateWalkFrameIfNeeded();
+			UpdateAttackFrameIfNeeded();
 
-			m_currState = Mouse.GetState();
-			CheckMouseoverState();
-			CheckMouseClickState();
-			m_prevState = m_currState;
+			_currMouseState = Mouse.GetState();
+			UpdateMouseoverName();
+			HandleLeftClick();
+			_prevMouseState = _currMouseState;
 
 			base.Update(gameTime);
-		}
-
-		private void UpdateDrawArea()
-		{
-			DrawArea = new Rectangle(
-				offX + 320 - World.Instance.MainPlayer.ActiveCharacter.OffsetX - (int)(baseFrame.Width / 6.4 * 3.2),
-				offY + 168 - World.Instance.MainPlayer.ActiveCharacter.OffsetY - baseFrame.Height,
-				_npcTextureFrameRectangle.Width, _npcTextureFrameRectangle.Height);
-		}
-
-		private void AnimateNPC(GameTime gameTime)
-		{
-			//switch the standing animation for NPCs every 500ms, if they're standing still
-			if (hasStandFrame1 && m_lastAnimUpdateTime.HasValue &&
-			    (gameTime.TotalGameTime - m_lastAnimUpdateTime.Value).TotalMilliseconds > 250)
-			{
-				if (Frame == NPCFrame.Standing)
-				{
-					Frame = NPCFrame.StandingFrame1;
-				}
-				else if (Frame == NPCFrame.StandingFrame1)
-				{
-					Frame = NPCFrame.Standing;
-				}
-				m_lastAnimUpdateTime = gameTime.TotalGameTime;
-			}
-			else if (!m_lastAnimUpdateTime.HasValue)
-			{
-				m_lastAnimUpdateTime = gameTime.TotalGameTime;
-			}
-		}
-
-		private void CheckMouseoverState()
-		{
-			if (_mouseoverName == null) return;
-
-			_mouseoverName.Visible = DrawArea.ContainsPoint(m_currState.X, m_currState.Y);
-			_mouseoverName.DrawLocation = new Vector2(
-				DrawArea.X + (DrawArea.Width - _mouseoverName.ActualWidth) / 2f,
-				DrawArea.Y + TopPixel - _mouseoverName.ActualHeight - 4);
-		}
-
-		private void CheckMouseClickState()
-		{
-			bool mouseClicked = m_currState.LeftButton == ButtonState.Released
-			                    && m_prevState.LeftButton == ButtonState.Pressed;
-
-			if (mouseClicked && DrawArea.ContainsPoint(m_currState.X, m_currState.Y))
-			{
-				if (World.Instance.MainPlayer.ActiveCharacter.NeedsSpellTarget)
-				{
-					SpellRecord data = World.Instance.ESF.GetSpellRecordByID((short) World.Instance.MainPlayer.ActiveCharacter.SelectedSpell);
-					if (data.TargetRestrict != SpellTargetRestrict.Friendly)
-					{
-						World.Instance.ActiveCharacterRenderer.SetSpellTarget(this);
-					}
-					else
-					{
-						//todo status label message "you cannot attack this NPC"
-						World.Instance.MainPlayer.ActiveCharacter.SelectSpell(-1);
-					}
-
-					return; //don't process regular click on NPC while targeting a spell
-				}
-
-				PacketAPI api = ((EOGame) Game).API;
-				switch (NPC.Data.Type)
-				{
-					case NPCType.Shop: ShopDialog.Show(api, this); break;
-					case NPCType.Inn: break;
-					case NPCType.Bank: BankAccountDialog.Show(api, NPC.Index); break;
-					case NPCType.Barber: break;
-					case NPCType.Guild: break;
-					case NPCType.Priest: break;
-					case NPCType.Law: break;
-					case NPCType.Skills: SkillmasterDialog.Show(api, NPC.Index); break;
-					case NPCType.Quest: QuestDialog.Show(api, NPC.Index, NPC.Data.VendorID, NPC.Data.Name); break;
-				}
-			}
 		}
 
 		public override void Draw(GameTime gameTime)
@@ -251,7 +125,7 @@ namespace EndlessClient.Rendering
 				? SpriteEffects.None
 				: SpriteEffects.FlipHorizontally;
 
-			if(!started)
+			if (!started)
 				batch.Begin();
 
 			Color col = Dying ? Color.FromNonPremultiplied(255, 255, 255, _fadeAwayAlpha -= 3) : Color.White;
@@ -266,65 +140,213 @@ namespace EndlessClient.Rendering
 				1f);
 
 			if (Dying && _fadeAwayAlpha <= 0)
-			{
-				if(!started) batch.End();
 				CompleteDeath = true;
-				return;
-			}
 
 			if (!started)
 				batch.End();
 		}
 
-		public new void Dispose()
-		{
-			Dispose(true);
-		}
+		#endregion
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				if (_walkTimer != null)
-				{
-					_walkTimer.Change(Timeout.Infinite, Timeout.Infinite);
-					_walkTimer.Dispose();
-					_walkTimer = null;
-				}
-
-				if(_sb != null)
-					_sb.Dispose();
-
-				if (m_chatBubble != null)
-					m_chatBubble.Dispose();
-
-				if (_mouseoverName != null)
-				{
-					_mouseoverName.Close();
-					_mouseoverName = null;
-				}
-			}
-
-			base.Dispose(disposing);
-		}
+		#region Public Interface
 
 		public void Walk(byte x, byte y, EODirection dir)
 		{
-			if (NPC.Walking || _walkTimer == null) return;
+			if (NPC.Walking) return;
 
-			//the direction is required for the walk callback
+			_actionStartTime = DateTime.Now;
 			NPC.BeginWalking(dir, x, y);
-			_walkTimer.Change(0, 100);
 		}
 
-		private void _walkCallback(object state)
+		public void Attack(EODirection dir)
 		{
+			if (NPC.Attacking) return;
+
+			_actionStartTime = DateTime.Now;
+			NPC.BeginAttacking(dir);
+		}
+
+		public void SetChatBubbleText(string message, bool isGroupChat)
+		{
+			_chatBubble.SetMessage(message, isGroupChat);
+		}
+
+		public void HideChatBubble()
+		{
+			_chatBubble.HideBubble();
+		}
+
+		public void TakeDamageFrom(Character opponent, int damage, int pctHealth)
+		{
+			m_damageCounter.SetValue(damage, pctHealth); //NPCs don't know heal spells
+			NPC.TakeDamage(opponent, damage);
+		}
+
+		public void FadeAway()
+		{
+			Dying = true;
+
+			if (_mouseoverName != null)
+			{
+				_mouseoverName.Close();
+				_mouseoverName = null;
+			}
+		}
+
+		#endregion
+
+		#region Helpers
+
+		private void InitializeTopPixel()
+		{
+			int tries;
+			for (tries = 0; tries < 3; ++tries)
+			{
+				try
+				{
+					//get the first non-transparent pixel to determine offsets for name labels and damage counters
+					Frame = NPCFrame.Standing;
+
+					var frameTexture = _npcSheet.GetNPCTexture();
+					var frameTextureData = new Color[frameTexture.Width * frameTexture.Height];
+					frameTexture.GetData(frameTextureData);
+
+					if (frameTextureData.All(x => x.A == 0))
+						TopPixel = 0;
+					else
+					{
+						var firstVisiblePixelIndex = frameTextureData.Select((color, index) => new { color, index })
+																	 .Where(x => x.color.R != 0)
+																	 .Select(x => x.index)
+																	 .First();
+						TopPixel = firstVisiblePixelIndex/frameTexture.Height;
+					}
+				} //this block throws errors sometimes..no idea why. It usually doesn't fail 3 times.
+				catch (InvalidOperationException) { continue; }
+
+				break;
+			}
+
+			if (tries >= 3)
+				throw new InvalidOperationException("Something weird happened initializing this NPC.");
+		}
+
+		private void InitializeStandingFrame1()
+		{
+			//attempt to get standing frame 1. It will have non-black pixels if it exists.
+			Frame = NPCFrame.StandingFrame1;
+
+			Texture2D frameTexture = _npcSheet.GetNPCTexture();
+			Color[] textureData = new Color[frameTexture.Width * frameTexture.Height];
+			frameTexture.GetData(textureData);
+
+			hasStandFrame1 = textureData.Any(_c => _c.R != 0 || _c.G != 0 || _c.B != 0);
+		}
+
+		private void CreateMouseoverName()
+		{
+			_mouseoverName = new XNALabel(new Rectangle(1, 1, 1, 1), Constants.FontSize08pt75)
+			{
+				Visible = false,
+				Text = NPC.Data.Name,
+				ForeColor = Color.White,
+				AutoSize = false,
+				DrawOrder = (int)ControlDrawLayer.BaseLayer + 3
+			};
+			_mouseoverName.DrawLocation = new Vector2(
+				DrawArea.X + (DrawArea.Width - _mouseoverName.ActualWidth) / 2f,
+				DrawArea.Y + TopPixel - _mouseoverName.ActualHeight - 4);
+			_mouseoverName.ResizeBasedOnText();
+		}
+
+		private void UpdateDrawArea()
+		{
+			DrawArea = new Rectangle(
+				DrawOffsetX + 320 - World.Instance.MainPlayer.ActiveCharacter.OffsetX - (int)(_npcTextureFrameRectangle.Width / 6.4 * 3.2),
+				DrawOffsetY + 168 - World.Instance.MainPlayer.ActiveCharacter.OffsetY - _npcTextureFrameRectangle.Height,
+				_npcTextureFrameRectangle.Width, _npcTextureFrameRectangle.Height);
+		}
+
+		private void UpdateStandingFrameIfNeeded()
+		{
+			if (NPC.Walking || NPC.Attacking || !hasStandFrame1)
+				return;
+
+			//switch the standing animation for NPCs every 500ms, if they're standing still
+			if ((DateTime.Now - _lastAnimUpdateTime).TotalMilliseconds > 250)
+			{
+				if (Frame == NPCFrame.Standing)
+				{
+					Frame = NPCFrame.StandingFrame1;
+				}
+				else if (Frame == NPCFrame.StandingFrame1)
+				{
+					Frame = NPCFrame.Standing;
+				}
+				_lastAnimUpdateTime = DateTime.Now;
+			}
+		}
+
+		private void UpdateMouseoverName()
+		{
+			if (_mouseoverName == null) return;
+
+			_mouseoverName.Visible = DrawArea.ContainsPoint(_currMouseState.X, _currMouseState.Y);
+			_mouseoverName.DrawLocation = new Vector2(
+				DrawArea.X + (DrawArea.Width - _mouseoverName.ActualWidth) / 2f,
+				DrawArea.Y + TopPixel - _mouseoverName.ActualHeight - 4);
+		}
+
+		private void HandleLeftClick()
+		{
+			bool mouseClicked = _currMouseState.LeftButton == ButtonState.Released
+								&& _prevMouseState.LeftButton == ButtonState.Pressed;
+
+			if (mouseClicked && DrawArea.ContainsPoint(_currMouseState.X, _currMouseState.Y))
+			{
+				if (World.Instance.MainPlayer.ActiveCharacter.NeedsSpellTarget)
+				{
+					SpellRecord data = World.Instance.ESF.GetSpellRecordByID((short)World.Instance.MainPlayer.ActiveCharacter.SelectedSpell);
+					if (data.TargetRestrict != SpellTargetRestrict.Friendly)
+					{
+						World.Instance.ActiveCharacterRenderer.SetSpellTarget(this);
+					}
+					else
+					{
+						//todo status label message "you cannot attack this NPC"
+						World.Instance.MainPlayer.ActiveCharacter.SelectSpell(-1);
+					}
+
+					return; //don't process regular click on NPC while targeting a spell
+				}
+
+				PacketAPI api = ((EOGame)Game).API;
+				switch (NPC.Data.Type)
+				{
+					case NPCType.Shop: ShopDialog.Show(api, this); break;
+					case NPCType.Inn: break;
+					case NPCType.Bank: BankAccountDialog.Show(api, NPC.Index); break;
+					case NPCType.Barber: break;
+					case NPCType.Guild: break;
+					case NPCType.Priest: break;
+					case NPCType.Law: break;
+					case NPCType.Skills: SkillmasterDialog.Show(api, NPC.Index); break;
+					case NPCType.Quest: QuestDialog.Show(api, NPC.Index, NPC.Data.VendorID, NPC.Data.Name); break;
+				}
+			}
+		}
+
+		private void UpdateWalkFrameIfNeeded()
+		{
+			if (!NPC.Walking || (DateTime.Now - _actionStartTime).TotalMilliseconds < 100)
+				return;
+
 			switch (NPC.Direction)
 			{
-				case EODirection.Down: adjX += -8; adjY += 4; break;
-				case EODirection.Left: adjX += -8; adjY += -4; break;
-				case EODirection.Up: adjX += 8; adjY += -4; break;
-				case EODirection.Right: adjX += 8; adjY += 4; break;
+				case EODirection.Down: walkingAdjustedX += -8; walkingAdjustedY += 4; break;
+				case EODirection.Left: walkingAdjustedX += -8; walkingAdjustedY += -4; break;
+				case EODirection.Up: walkingAdjustedX += 8; walkingAdjustedY += -4; break;
+				case EODirection.Right: walkingAdjustedX += 8; walkingAdjustedY += 4; break;
 			}
 
 			switch (Frame)
@@ -344,23 +366,19 @@ namespace EndlessClient.Rendering
 					break;
 				case NPCFrame.WalkFrame4:
 					Frame = NPCFrame.Standing;
-					if (_walkTimer == null) return;
-					_walkTimer.Change(Timeout.Infinite, Timeout.Infinite);
 					NPC.EndWalking();
-					adjX = adjY = 0;
+					walkingAdjustedX = walkingAdjustedY = 0;
 					break;
 			}
+
+			_actionStartTime = DateTime.Now;
 		}
 
-		public void Attack(EODirection dir)
+		private void UpdateAttackFrameIfNeeded()
 		{
-			if (NPC.Attacking) return;
-			_attackTimer.Change(0, 100);
-			NPC.BeginAttacking(dir);
-		}
+			if (!NPC.Attacking || (DateTime.Now - _actionStartTime).TotalMilliseconds < 100)
+				return;
 
-		private void _attackCallback(object state)
-		{
 			switch (Frame)
 			{
 				case NPCFrame.Standing:
@@ -372,37 +390,33 @@ namespace EndlessClient.Rendering
 					break;
 				case NPCFrame.Attack2:
 					Frame = NPCFrame.Standing;
-					_attackTimer.Change(Timeout.Infinite, Timeout.Infinite);
 					NPC.EndAttacking();
 					break;
 			}
+
+			_actionStartTime = DateTime.Now;
 		}
 
-		public void SetChatBubbleText(string message, bool isGroupChat)
-		{
-			m_chatBubble.SetMessage(message, isGroupChat);
-		}
+		#endregion
 
-		public void HideChatBubble()
+		protected override void Dispose(bool disposing)
 		{
-			m_chatBubble.HideBubble();
-		}
-
-		public void TakeDamageFrom(Character opponent, int damage, int pctHealth)
-		{
-			m_damageCounter.SetValue(damage, pctHealth); //NPCs don't know heal spells
-			NPC.TakeDamage(opponent, damage);
-		}
-
-		public void FadeAway()
-		{
-			Dying = true;
-
-			if (_mouseoverName != null)
+			if (disposing)
 			{
-				_mouseoverName.Close();
-				_mouseoverName = null;
+				if (_sb != null)
+					_sb.Dispose();
+
+				if (_chatBubble != null)
+					_chatBubble.Dispose();
+
+				if (_mouseoverName != null)
+				{
+					_mouseoverName.Close();
+					_mouseoverName = null;
+				}
 			}
+
+			base.Dispose(disposing);
 		}
 	}
 }
