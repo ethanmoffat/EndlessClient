7d8cee0e2f8e26db36b3f90bc8b626036267ced1
Ethan Moffat <ethanmoffat@hotmail.com>
2016-03-04 16:52:21 -0800

    Add CharacterRenderProperties + interface

---
+++ EOLib/Data/BLL/CharacterRenderProperties.cs
@@ -0,0 +1,163 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+namespace EOLib.Data.BLL
+{
+	public class CharacterRenderProperties : ICharacterRenderProperties
+	{
+		public const int MAX_NUMBER_OF_WALK_FRAMES   = 4;
+		public const int MAX_NUMBER_OF_ATTACK_FRAMES = 2;
+		public const int MAX_NUMBER_OF_EMOTE_FRAMES  = 3;
+
+		public byte HairStyle { get; private set; }
+		public byte HairColor { get; private set; }
+		public byte Race { get; private set; }
+		public byte Gender { get; private set; }
+
+		public short BootsGraphic { get; private set; }
+		public short ArmorGraphic { get; private set; }
+		public short HatGraphic { get; private set; }
+		public short ShieldGraphic { get; private set; }
+		public short WeaponGraphic { get; private set; }
+
+		public EODirection Direction { get; private set; }
+
+		public int WalkFrame { get; private set; }
+		public int AttackFrame { get; private set; }
+		public int EmoteFrame { get; private set; }
+
+		public bool IsHidden { get; private set; }
+		public bool IsDead { get; private set; }
+
+		public ICharacterRenderProperties WithHairStyle(byte newHairStyle)
+		{
+			var props = MakeCopy(this);
+			props.HairStyle = newHairStyle;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithHairColor(byte newHairColor)
+		{
+			var props = MakeCopy(this);
+			props.HairColor = newHairColor;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithRace(byte newRace)
+		{
+			var props = MakeCopy(this);
+			props.Race = newRace;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithGender(byte newGender)
+		{
+			var props = MakeCopy(this);
+			props.Gender = newGender;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithBootsGraphic(short bootsGraphic)
+		{
+			var props = MakeCopy(this);
+			props.BootsGraphic = bootsGraphic;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithArmorGraphic(short armorGraphic)
+		{
+			var props = MakeCopy(this);
+			props.ArmorGraphic = armorGraphic;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithHatGraphic(short hatGraphic)
+		{
+			var props = MakeCopy(this);
+			props.HatGraphic = hatGraphic;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithShieldGraphic(short shieldGraphic)
+		{
+			var props = MakeCopy(this);
+			props.ShieldGraphic = shieldGraphic;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithWeaponGraphic(short weaponGraphic)
+		{
+			var props = MakeCopy(this);
+			props.WeaponGraphic = weaponGraphic;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithDirection(EODirection newDirection)
+		{
+			var props = MakeCopy(this);
+			props.Direction = newDirection;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithNextWalkFrame()
+		{
+			var props = MakeCopy(this);
+			props.WalkFrame = (props.WalkFrame + 1) % MAX_NUMBER_OF_WALK_FRAMES;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithNextAttackFrame()
+		{
+			var props = MakeCopy(this);
+			props.AttackFrame = (props.AttackFrame + 1) % MAX_NUMBER_OF_ATTACK_FRAMES;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithNextEmoteFrame()
+		{
+			var props = MakeCopy(this);
+			props.EmoteFrame = (props.EmoteFrame + 1) % MAX_NUMBER_OF_EMOTE_FRAMES;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithIsHidden(bool hidden)
+		{
+			var props = MakeCopy(this);
+			props.IsHidden = hidden;
+			return props;
+		}
+
+		public ICharacterRenderProperties WithDead()
+		{
+			var props = MakeCopy(this);
+			props.IsDead = true;
+			return props;
+		}
+
+		private static CharacterRenderProperties MakeCopy(ICharacterRenderProperties other)
+		{
+			return new CharacterRenderProperties
+			{
+				HairStyle = other.HairStyle,
+				HairColor = other.HairColor,
+				Race = other.Race,
+				Gender = other.Gender,
+
+				BootsGraphic = other.BootsGraphic,
+				ArmorGraphic = other.ArmorGraphic,
+				HatGraphic = other.HatGraphic,
+				ShieldGraphic = other.ShieldGraphic,
+				WeaponGraphic = other.WeaponGraphic,
+
+				Direction = other.Direction,
+				WalkFrame = other.WalkFrame,
+				AttackFrame = other.AttackFrame,
+				EmoteFrame = other.EmoteFrame,
+
+				IsHidden = other.IsHidden,
+				IsDead = other.IsDead
+			};
+		}
+	}
+}
---
+++ EOLib/Data/BLL/ICharacterRenderProperties.cs
@@ -0,0 +1,49 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+namespace EOLib.Data.BLL
+{
+	public interface ICharacterRenderProperties
+	{
+		byte HairStyle { get; }
+		byte HairColor { get; }
+		byte Race { get; }
+		byte Gender { get; }
+
+		short BootsGraphic { get; }
+		short ArmorGraphic { get; }
+		short HatGraphic { get; }
+		short ShieldGraphic { get; }
+		short WeaponGraphic { get; }
+
+		EODirection Direction { get; }
+
+		int WalkFrame { get; }
+		int AttackFrame { get; }
+		int EmoteFrame { get; }
+
+		bool IsHidden { get; }
+		bool IsDead { get; }
+
+		ICharacterRenderProperties WithHairStyle(byte newHairStyle);
+		ICharacterRenderProperties WithHairColor(byte newHairColor);
+		ICharacterRenderProperties WithRace(byte newRace);
+		ICharacterRenderProperties WithGender(byte newGender);
+
+		ICharacterRenderProperties WithBootsGraphic(short bootsGraphic);
+		ICharacterRenderProperties WithArmorGraphic(short armorGraphic);
+		ICharacterRenderProperties WithHatGraphic(short hatGraphic);
+		ICharacterRenderProperties WithShieldGraphic(short shieldGraphic);
+		ICharacterRenderProperties WithWeaponGraphic(short weaponGraphic);
+
+		ICharacterRenderProperties WithDirection(EODirection newDirection);
+
+		ICharacterRenderProperties WithNextWalkFrame();
+		ICharacterRenderProperties WithNextAttackFrame();
+		ICharacterRenderProperties WithNextEmoteFrame();
+
+		ICharacterRenderProperties WithIsHidden(bool hidden);
+		ICharacterRenderProperties WithDead();
+	}
+}
