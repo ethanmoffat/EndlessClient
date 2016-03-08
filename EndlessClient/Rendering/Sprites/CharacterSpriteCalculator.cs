1291032ccfa8a695c6ada74eaf2d3a6d00a4a7b2
Ethan Moffat <ethanmoffat@hotmail.com>
2016-03-07 16:47:48 -0800

    Armor logic -> CharacterSpriteCalculator

--- EndlessClient/Rendering/Sprites/CharacterSpriteCalculator.cs
+++ EndlessClient/Rendering/Sprites/CharacterSpriteCalculator.cs
@@ -60,7 +60,59 @@ namespace EndlessClient.Rendering.Sprites
 
 		public ISpriteSheet GetArmorTexture(bool isBow)
 		{
-			throw new System.NotImplementedException();
+			ArmorShieldSpriteType type = ArmorShieldSpriteType.Standing;
+			switch (_characterRenderProperties.CurrentAction)
+			{
+				case CharacterActionState.Walking:
+					switch (_characterRenderProperties.WalkFrame)
+					{
+						case 1: type = ArmorShieldSpriteType.WalkFrame1; break;
+						case 2: type = ArmorShieldSpriteType.WalkFrame2; break;
+						case 3: type = ArmorShieldSpriteType.WalkFrame3; break;
+						case 4: type = ArmorShieldSpriteType.WalkFrame4; break;
+					}
+					break;
+				case CharacterActionState.Attacking:
+					if (isBow)
+					{
+						switch (_characterRenderProperties.AttackFrame)
+						{
+							case 1: type = ArmorShieldSpriteType.Bow; break;
+							case 2: type = ArmorShieldSpriteType.Standing; break;
+						}
+					}
+					else
+					{
+						switch (_characterRenderProperties.AttackFrame)
+						{
+							case 1: type = ArmorShieldSpriteType.PunchFrame1; break;
+							case 2: type = ArmorShieldSpriteType.PunchFrame2; break;
+						}
+					}
+					break;
+				case CharacterActionState.SpellCast:
+					type = ArmorShieldSpriteType.SpellCast;
+					break;
+				case CharacterActionState.Sitting:
+					switch (_characterRenderProperties.SitState)
+					{
+						case SitState.Chair:
+							type = ArmorShieldSpriteType.SitChair;
+							break;
+						case SitState.Floor:
+							type = ArmorShieldSpriteType.SitGround;
+							break;
+					}
+					break;
+			}
+
+			var gfxFile = _characterRenderProperties.Gender == 0 ? GFXTypes.FemaleArmor : GFXTypes.MaleArmor;
+
+			var offset = GetOffsetBasedOnState(type) * GetBaseFactorFromDirection();
+			var baseArmorValue = GetBaseArmorGraphic();
+			var gfxNumber = baseArmorValue + (int)type + offset;
+
+			return new SpriteSheet(_gfxManager.TextureFromResource(gfxFile, gfxNumber, true));
 		}
 
 		public ISpriteSheet GetHatTexture()
@@ -100,7 +152,12 @@ namespace EndlessClient.Rendering.Sprites
 
 		private short GetBaseBootGraphic()
 		{
-			return (short)((_characterRenderProperties.BootsGraphic - 1) * 40);
+			return (short) ((_characterRenderProperties.BootsGraphic - 1)*40);
+		}
+
+		private short GetBaseArmorGraphic()
+		{
+			return (short) ((_characterRenderProperties.ArmorGraphic - 1)*50);
 		}
 
 		private int GetBaseFactorFromDirection()
@@ -118,6 +175,22 @@ namespace EndlessClient.Rendering.Sprites
 				case BootsSpriteType.WalkFrame3:
 				case BootsSpriteType.WalkFrame4:
 					return 4;
+			}
+			return 1;
+		}
+
+		private int GetOffsetBasedOnState(ArmorShieldSpriteType type)
+		{
+			switch (type)
+			{
+				case ArmorShieldSpriteType.WalkFrame1:
+				case ArmorShieldSpriteType.WalkFrame2:
+				case ArmorShieldSpriteType.WalkFrame3:
+				case ArmorShieldSpriteType.WalkFrame4:
+					return 4;
+				case ArmorShieldSpriteType.PunchFrame1:
+				case ArmorShieldSpriteType.PunchFrame2:
+					return 2;
 			}
 			return 1;
 		}
