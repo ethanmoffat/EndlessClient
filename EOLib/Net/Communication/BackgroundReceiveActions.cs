c31a88d3504c125e59d6987c4693700bfe816d1e
Ethan Moffat <ethanmoffat@hotmail.com>
2016-04-01 16:10:47 -0700

    Make BackgroundReceiveLoop run on a different thread

--- EOLib/Net/Communication/BackgroundReceiveActions.cs
+++ EOLib/Net/Communication/BackgroundReceiveActions.cs
@@ -2,7 +2,7 @@
 // This file is subject to the GPL v2 License
 // For additional details, see the LICENSE file
 
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace EOLib.Net.Communication
 {
@@ -10,19 +10,29 @@ namespace EOLib.Net.Communication
 	{
 		private readonly INetworkClientProvider _clientProvider;
 
+		private Thread _backgroundThread;
+
 		public BackgroundReceiveActions(INetworkClientProvider clientProvider)
 		{
 			_clientProvider = clientProvider;
+			_backgroundThread = new Thread(BackgroundLoop);
 		}
 
-		public async Task RunBackgroundReceiveLoopAsync()
+		public void RunBackgroundReceiveLoop()
 		{
-			await Client.RunReceiveLoopAsync();
+			_backgroundThread.Start();
 		}
 
 		public void CancelBackgroundReceiveLoop()
 		{
 			Client.CancelBackgroundReceiveLoop();
+			_backgroundThread.Join();
+			_backgroundThread = new Thread(BackgroundLoop);
+		}
+
+		private async void BackgroundLoop()
+		{
+			await Client.RunReceiveLoopAsync();
 		}
 
 		private INetworkClient Client { get { return _clientProvider.NetworkClient; } }
--- EOLib/Net/Communication/IBackgroundReceiveActions.cs
+++ EOLib/Net/Communication/IBackgroundReceiveActions.cs
@@ -2,13 +2,11 @@
 // This file is subject to the GPL v2 License
 // For additional details, see the LICENSE file
 
-using System.Threading.Tasks;
-
 namespace EOLib.Net.Communication
 {
 	public interface IBackgroundReceiveActions
 	{
-		Task RunBackgroundReceiveLoopAsync();
+		void RunBackgroundReceiveLoop();
 
 		void CancelBackgroundReceiveLoop();
 	}
