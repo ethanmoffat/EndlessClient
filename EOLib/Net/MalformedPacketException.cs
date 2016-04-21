4b259afd0fc7f8c58b92cff2747fa930bae0fbc6
Ethan Moffat <ethanmoffat@hotmail.com>
2016-04-21 15:40:26 -0700

    Add packet translator for login data

---
+++ EOLib/Net/MalformedPacketException.cs
@@ -0,0 +1,19 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System;
+
+namespace EOLib.Net
+{
+	public class MalformedPacketException : Exception
+	{
+		public IPacket Packet { get; private set; }
+
+		public MalformedPacketException(string message, IPacket packet)
+			: base(message)
+		{
+			Packet = packet;
+		}
+	}
+}
---
+++ EOLib/Net/Translators/AccountLoginPacketTranslator.cs
@@ -0,0 +1,66 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System.Collections.Generic;
+using System.IO;
+using EOLib.Data.BLL;
+using EOLib.Data.Login;
+using EOLib.Net.API;
+
+namespace EOLib.Net.Translators
+{
+	public class AccountLoginPacketTranslator : IPacketTranslator<IAccountLoginData>
+	{
+		public IAccountLoginData TranslatePacket(IPacket packet)
+		{
+			var reply = (LoginReply)packet.ReadShort();
+
+			var characters = new List<ICharacter>();
+			if (reply == LoginReply.Ok)
+			{
+
+				var numberOfCharacters = (int) packet.ReadChar();
+				packet.Seek(2, SeekOrigin.Current);
+
+				for (int i = 0; i < numberOfCharacters; ++i)
+				{
+					characters.Add(GetNextCharacter(packet));
+					if(packet.ReadChar() != 255)
+						throw new MalformedPacketException("Login packet missing character separator byte", packet);
+				}
+			}
+
+			return new AccountLoginData(reply, characters);
+		}
+
+		private ICharacter GetNextCharacter(IPacket packet)
+		{
+			ICharacter character = new Character()
+				.WithName(packet.ReadBreakString())
+				.WithID(packet.ReadInt());
+
+			var stats = new CharacterStats()
+				.WithNewStat(CharacterStat.Level, packet.ReadChar());
+
+			var renderProperties = new CharacterRenderProperties()
+				.WithGender(packet.ReadChar())
+				.WithHairStyle(packet.ReadChar())
+				.WithHairColor(packet.ReadChar())
+				.WithRace(packet.ReadChar());
+
+			character = character.WithAdminLevel((AdminLevel) packet.ReadChar());
+
+			renderProperties = renderProperties
+				.WithBootsGraphic(packet.ReadShort())
+				.WithArmorGraphic(packet.ReadShort())
+				.WithHatGraphic(packet.ReadShort())
+				.WithShieldGraphic(packet.ReadShort())
+				.WithWeaponGraphic(packet.ReadShort());
+
+			return character
+				.WithRenderProperties(renderProperties)
+				.WithStats(stats);
+		}
+	}
+}
