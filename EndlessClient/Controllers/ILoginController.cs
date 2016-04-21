a23d65007b5befbc9b217a5ba3043e3ffdd1c89d
Ethan Moffat <ethanmoffat@hotmail.com>
2016-04-20 22:09:10 -0700

    Some initial files for logging in

---
+++ EndlessClient/Controllers/ILoginController.cs
@@ -0,0 +1,16 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System.Threading.Tasks;
+using EOLib.Data.Login;
+
+namespace EndlessClient.Controllers
+{
+	public interface ILoginController
+	{
+		Task LoginToAccount(ILoginParameters loginParameters);
+
+		Task LoginToCharacter(); //todo: parameter for which character
+	}
+}
