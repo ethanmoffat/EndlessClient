6c7d35a6fc2d9c208941d4d5a08048c52185f3ed
Ethan Moffat <ethanmoffat@hotmail.com>
2016-04-29 15:38:11 -0700

    Add CharacterForDelete to CharacterSelectorRepository

--- EOLib/Domain/Login/ICharacterSelectorRepository.cs
+++ EOLib/Domain/Login/ICharacterSelectorRepository.cs
@@ -10,15 +10,21 @@ namespace EOLib.Domain.Login
 	public interface ICharacterSelectorRepository
 	{
 		IReadOnlyList<ICharacter> Characters { get; set; }
+
+		ICharacter CharacterForDelete { get; set; }
 	}
 
 	public interface ICharacterSelectorProvider
 	{
 		IReadOnlyList<ICharacter> Characters { get; }
+
+		ICharacter CharacterForDelete { get; }
 	}
 
 	public class CharacterSelectorRepository : ICharacterSelectorRepository, ICharacterSelectorProvider
 	{
 		public IReadOnlyList<ICharacter> Characters { get; set; }
+
+		public ICharacter CharacterForDelete { get; set; }
 	}
 }
