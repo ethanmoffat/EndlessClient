62d0c6136758ba40f31e3c712b8e613289b0279e
Ethan Moffat <ethanmoffat@hotmail.com>
2016-05-02 11:31:33 -0700

    Implementation for PacketHandlingTypeFinder

--- EOLib/Net/Handlers/PacketHandlingType.cs
+++ EOLib/Net/Handlers/PacketHandlingType.cs
@@ -7,6 +7,7 @@ namespace EOLib.Net.Handlers
 	public enum PacketHandlingType
 	{
 		InBand,
-		OutOfBand
+		OutOfBand,
+		NotHandled
 	}
 }
--- EOLib/Net/Handlers/PacketHandlingTypeFinder.cs
+++ EOLib/Net/Handlers/PacketHandlingTypeFinder.cs
@@ -2,13 +2,42 @@
 // This file is subject to the GPL v2 License
 // For additional details, see the LICENSE file
 
+using System.Collections.Generic;
+
 namespace EOLib.Net.Handlers
 {
 	public class PacketHandlingTypeFinder : IPacketHandlingTypeFinder
 	{
+		private readonly List<FamilyActionPair> _inBandPackets;
+		private readonly List<FamilyActionPair> _outOfBandPackets;
+
+		public PacketHandlingTypeFinder()
+		{
+			_inBandPackets = new List<FamilyActionPair>
+			{
+				new FamilyActionPair(PacketFamily.Init, PacketAction.Init),
+				new FamilyActionPair(PacketFamily.Account, PacketAction.Reply),
+				new FamilyActionPair(PacketFamily.Character, PacketAction.Reply),
+				new FamilyActionPair(PacketFamily.Login, PacketAction.Reply)
+			};
+
+			_outOfBandPackets = new List<FamilyActionPair>
+			{
+				new FamilyActionPair(PacketFamily.Connection, PacketAction.Player)
+			};
+		}
+
 		public PacketHandlingType FindHandlingType(PacketFamily family, PacketAction action)
 		{
-			throw new System.NotImplementedException();
+			var fap = new FamilyActionPair(family, action);
+
+			if (_inBandPackets.Contains(fap))
+				return PacketHandlingType.InBand;
+
+			if (_outOfBandPackets.Contains(fap))
+				return PacketHandlingType.OutOfBand;
+
+			return PacketHandlingType.NotHandled;
 		}
 	}
 }
