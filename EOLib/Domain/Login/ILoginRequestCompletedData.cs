8e1f4f782b2d57ea6238c1789b8e118af8448b13
Ethan Moffat <ethanmoffat@hotmail.com>
2016-05-05 16:15:55 -0700

    Add LoginRequestCompletedData, complete implementation of translator

--- EOLib/Domain/Login/ILoginRequestCompletedData.cs
+++ EOLib/Domain/Login/ILoginRequestCompletedData.cs
@@ -2,11 +2,46 @@
 // This file is subject to the GPL v2 License
 // For additional details, see the LICENSE file
 
+using System.Collections.Generic;
+using EOLib.Domain.Character;
+using EOLib.Domain.Map;
+using EOLib.Domain.NPC;
 using EOLib.Net.Translators;
 
 namespace EOLib.Domain.Login
 {
 	public interface ILoginRequestCompletedData : ITranslatedData
 	{
+		IReadOnlyList<string> News { get; }
+
+		byte CharacterWeight { get; }
+
+		byte CharacterMaxWeight { get; }
+
+		IReadOnlyList<IInventoryItem> CharacterItemInventory { get; }
+
+		IReadOnlyList<IInventorySpell> CharacterSpellInventory { get; }
+
+		IReadOnlyList<ICharacter> MapCharacters { get; }
+
+		IReadOnlyList<INPC> MapNPCs { get; }
+
+		IReadOnlyList<IMapItem> MapItems { get; }
+
+		ILoginRequestCompletedData WithNews(IEnumerable<string> newsStrings);
+
+		ILoginRequestCompletedData WithWeight(byte weight);
+
+		ILoginRequestCompletedData WithMaxWeight(byte maxWeight);
+
+		ILoginRequestCompletedData WithInventory(IEnumerable<IInventoryItem> inventoryItems);
+
+		ILoginRequestCompletedData WithSpells(IEnumerable<IInventorySpell> inventorySpells);
+
+		ILoginRequestCompletedData WithCharacters(IEnumerable<ICharacter> characters);
+
+		ILoginRequestCompletedData WithNPCs(IEnumerable<INPC> npcs);
+
+		ILoginRequestCompletedData WithItems(IEnumerable<IMapItem> items);
 	}
 }
--- EOLib/Net/Translators/LoginRequestCompletedPacketTranslator.cs
+++ EOLib/Net/Translators/LoginRequestCompletedPacketTranslator.cs
@@ -2,7 +2,6 @@
 // This file is subject to the GPL v2 License
 // For additional details, see the LICENSE file
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using EOLib.Domain.Character;
@@ -20,6 +19,9 @@ namespace EOLib.Net.Translators
 			if (reply != CharacterLoginReply.RequestCompleted)
 				throw new MalformedPacketException("Unexpected welcome response in packet: " + reply, packet);
 
+			if (packet.ReadByte() != 255)
+				throw new MalformedPacketException("Missing 255 byte separator after CharacterLoginReply type", packet);
+
 			var news = GetNews(packet).ToList();
 
 			var weight = packet.ReadChar();
@@ -27,15 +29,26 @@ namespace EOLib.Net.Translators
 
 			var inventoryItems = GetInventoryItems(packet).ToList();
 			var inventorySpells = GetInventorySpells(packet).ToList();
-			throw new InvalidOperationException();
+
+			var characters = GetCharacters(packet).ToList();
+			var npcs = GetNPCs(packet).ToList();
+			var items = GetMapItems(packet).ToList();
+
+			return new LoginRequestCompletedData()
+				.WithNews(news)
+				.WithWeight(weight)
+				.WithMaxWeight(maxWeight)
+				.WithInventory(inventoryItems)
+				.WithSpells(inventorySpells)
+				.WithCharacters(characters)
+				.WithNPCs(npcs)
+				.WithItems(items);
 		}
 
 		private IEnumerable<string> GetNews(IPacket packet)
 		{
 			for (int i = 0; i < MAX_NEWS_LINES; ++i)
-			{
 				yield return packet.ReadBreakString();
-			}
 		}
 
 		private IEnumerable<IInventoryItem> GetInventoryItems(IPacket packet)
@@ -48,11 +61,10 @@ namespace EOLib.Net.Translators
 
 		private IEnumerable<IInventorySpell> GetInventorySpells(IPacket packet)
 		{
-			return null;
-			//while (packet.PeekByte() != 255)
-			//	yield return new InventorySpell(packet.ReadShort(), packet.ReadShort());
+			while (packet.PeekByte() != 255)
+				yield return new InventorySpell(packet.ReadShort(), packet.ReadShort());
 
-			//packet.ReadByte();
+			packet.ReadByte();
 		}
 	}
 }
