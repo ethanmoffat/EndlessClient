ba8a9afaaef6a52d73d807038637aff777a48dbb
Ethan Moffat <ethanmoffat@hotmail.com>
2016-05-06 16:38:48 -0700

    Add UnityExtensions

---
+++ EOLib/UnityExtensions.cs
@@ -0,0 +1,46 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+
+namespace EOLib
+{
+	public static class UnityExtensions
+	{
+		public static void RegisterInstance<T>(this UnityContainer container)
+		{
+			container.RegisterType<T>(new ContainerControlledLifetimeManager());
+		}
+
+		public static void RegisterInstance<T, U>(this UnityContainer container) where U : T
+		{
+			container.RegisterType<T, U>(new ContainerControlledLifetimeManager());
+		}
+
+		public static void RegisterVaried<T, U>(this UnityContainer container) where U : T
+		{
+			RegisterEnumerableIfNeeded<T, U>(container);
+
+			container.RegisterType<T, U>(typeof(U).Name);
+		}
+
+		public static void RegisterInstanceVaried<T, U>(this UnityContainer container) where U : T
+		{
+			RegisterEnumerableIfNeeded<T, U>(container);
+
+			container.RegisterType<T, U>(typeof(U).Name, new ContainerControlledLifetimeManager());
+		}
+
+		private static void RegisterEnumerableIfNeeded<T, U>(UnityContainer container) where U : T
+		{
+			if (!container.IsRegistered(typeof(IEnumerable<T>)))
+			{
+				container.RegisterType<IEnumerable<T>>(
+					new ContainerControlledLifetimeManager(),
+					new InjectionFactory(c => c.ResolveAll<T>()));
+			}
+		}
+	}
+}
