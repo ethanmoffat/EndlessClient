d841559a8f1973dcd03042992cb567a27738fd3d
Ethan Moffat <ethanmoffat@hotmail.com>
2016-07-18 20:03:42 -0700

    First pass of tests for FileRequestService

---
+++ EOLib.Test/TestHelpers/PacketSendServiceHelpers.cs
@@ -0,0 +1,34 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System.Threading.Tasks;
+using EOLib.Net;
+using EOLib.Net.Communication;
+using Moq;
+
+namespace EOLib.Test.TestHelpers
+{
+    internal static class PacketSendServiceHelpers
+    {
+        /// <summary>
+        /// Setup the PacketSendService mock to return a packet with the specified family/action/data from SendEncodedPacketAndWaitAsync
+        /// </summary>
+        /// <param name="packetSendServiceMock">The mocked packet send service</param>
+        /// <param name="family">Packet family for the "received" packet</param>
+        /// <param name="action">Packet action for the "received" packet</param>
+        /// <param name="data">Packet data payload (any additional data that should be in the packet)</param>
+        internal static void SetupReceivedPacketHasHeader(this Mock<IPacketSendService> packetSendServiceMock,
+                                                          PacketFamily family,
+                                                          PacketAction action,
+                                                          params byte[] data)
+        {
+            var receivedPacket = new PacketBuilder(family, action)
+                .AddBytes(data)
+                .Build();
+
+            packetSendServiceMock.Setup(x => x.SendEncodedPacketAndWaitAsync(It.IsAny<IPacket>()))
+                                 .Returns(Task.FromResult(receivedPacket));
+        }
+    }
+}
