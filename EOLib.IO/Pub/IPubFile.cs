c2080d2cabaad4d5365bc0880fb2392a82d92c23
Ethan Moffat <ethanmoffat@hotmail.com>
2016-07-19 13:36:13 -0700

    Add initial interfaces for Pub files to EOLib.IO.Pub

---
+++ EOLib.IO/Pub/IPubFile.cs
@@ -0,0 +1,23 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+namespace EOLib.IO.Pub
+{
+    public interface IPubFile<TRecord> where TRecord : IPubRecord
+    {
+        string FileType { get; }
+
+        int CheckSum { get; set; }
+
+        int Length { get; }
+
+        int Version { get; set; }
+
+        TRecord this[int id] { get; set; }
+
+        byte[] SerializeToByteArray();
+
+        void CreateFromByteArray(byte[] bytes);
+    }
+}
---
+++ EOLib.IO/Pub/IPubRecord.cs
@@ -0,0 +1,11 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+namespace EOLib.IO.Pub
+{
+    public interface IPubRecord
+    {
+
+    }
+}
