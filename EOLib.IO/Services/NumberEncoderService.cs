bc7d8052c308cb714272616b7eaf098eb3800ca0
Ethan Moffat <ethanmoffat@hotmail.com>
2016-07-20 15:47:55 -0700

    Refactor NumberEncoderService to match algorithms in EOSERV source more closely
    The way the algorithms are written in EOSERV is much more readable. I also got the original algorithms from a decompiled C# DLL, so I'll let the compiler do any further optimization.

--- EOLib.IO/Services/NumberEncoderService.cs
+++ EOLib.IO/Services/NumberEncoderService.cs
@@ -2,30 +2,40 @@
 // This file is subject to the GPL v2 License
 // For additional details, see the LICENSE file
 
+using System.Linq;
+
 namespace EOLib.IO.Services
 {
     public class NumberEncoderService : INumberEncoderService
     {
         public byte[] EncodeNumber(int number, int size)
         {
-            byte[] numArray = new byte[size];
-            for (int index = 3; index >= 1; --index)
+            var numArray = Enumerable.Repeat(254, 4).ToArray();
+            var original = number;
+
+            if (original >= NumericConstants.THREE_BYTE_MAX)
             {
-                if (index >= numArray.Length)
-                {
-                    if (number >= NumericConstants.NUMERIC_MAXIMUM[index - 1])
-                        number %= NumericConstants.NUMERIC_MAXIMUM[index - 1];
-                }
-                else if (number >= NumericConstants.NUMERIC_MAXIMUM[index - 1])
-                {
-                    numArray[index] = (byte)(number / NumericConstants.NUMERIC_MAXIMUM[index - 1] + 1);
-                    number %= NumericConstants.NUMERIC_MAXIMUM[index - 1];
-                }
-                else
-                    numArray[index] = 254;
+                numArray[3] = number/NumericConstants.THREE_BYTE_MAX + 1;
+                number = number%NumericConstants.THREE_BYTE_MAX;
             }
-            numArray[0] = (byte)(number + 1);
-            return numArray;
+
+            if (original >= NumericConstants.TWO_BYTE_MAX)
+            {
+                numArray[2] = number/NumericConstants.TWO_BYTE_MAX + 1;
+                number = number%NumericConstants.TWO_BYTE_MAX;
+            }
+
+            if (original >= NumericConstants.ONE_BYTE_MAX)
+            {
+                numArray[1] = number/NumericConstants.ONE_BYTE_MAX + 1;
+                number = number%NumericConstants.ONE_BYTE_MAX;
+            }
+
+            numArray[0] = number + 1;
+
+            return numArray.Select(x => (byte)x)
+                           .Take(size)
+                           .ToArray();
         }
 
         public int DecodeNumber(params byte[] b)
@@ -39,10 +49,15 @@ namespace EOLib.IO.Services
                 --b[index];
             }
 
-            int num = 0;
-            for (int index = b.Length - 1; index >= 1; --index)
-                num += b[index] * NumericConstants.NUMERIC_MAXIMUM[index - 1];
-            return num + b[0];
+            var retNum = 0;
+            if (b.Length > 3)
+                retNum += b[3]*NumericConstants.THREE_BYTE_MAX;
+            if (b.Length > 2)
+                retNum += b[2]*NumericConstants.TWO_BYTE_MAX;
+            if (b.Length > 1)
+                retNum += b[1]*NumericConstants.ONE_BYTE_MAX;
+
+            return retNum + b[0];
         }
     }
 }
