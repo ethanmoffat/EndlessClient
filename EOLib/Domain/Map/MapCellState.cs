73262275c82ceb3ec84ba905a268affbc16927c8
Ethan Moffat <ethanmoffat@hotmail.com>
2016-07-24 14:21:39 -0700

    Add MapCellState implementation

---
+++ EOLib/Domain/Map/MapCellState.cs
@@ -0,0 +1,45 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using System.Collections.Generic;
+using EOLib.Domain.Character;
+using EOLib.IO.Map;
+
+namespace EOLib.Domain.Map
+{
+    public class MapCellState : IMapCellState
+    {
+        public TileInfoReturnType ReturnType { get; private set; }
+
+        public IReadOnlyList<IMapItem> Items { get; private set; }
+
+        public TileSpec TileSpec { get; set; }
+
+        public Optional<IMapNPC> NPC { get; set; }
+
+        public Optional<ICharacter> Character { get; set; }
+
+        public Optional<IMapChest> Chest { get; set; }
+
+        public Optional<IMapWarp> Warp { get; set; }
+
+        public Optional<IMapSign> Sign { get; set; }
+
+        public MapCellState()
+        {
+            Items = new List<IMapItem>();
+            TileSpec = TileSpec.None;
+            NPC = new Optional<IMapNPC>();
+            Character = new Optional<ICharacter>();
+            Chest = new Optional<IMapChest>();
+            Warp = new Optional<IMapWarp>();
+            Sign = new Optional<IMapSign>();
+        }
+
+        public void SetItems(List<IMapItem> items)
+        {
+            Items = new List<IMapItem>(items);
+        }
+    }
+}
