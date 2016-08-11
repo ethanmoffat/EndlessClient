e285435e2c2d5f6796953e43f659f1189d20edc7
Ethan Moffat <ethanmoffat@hotmail.com>
2016-08-11 16:44:49 -0700

    PictureBox has a source rectangle

--- EndlessClient/UIControls/PictureBox.cs
+++ EndlessClient/UIControls/PictureBox.cs
@@ -2,6 +2,7 @@
 // This file is subject to the GPL v2 License
 // For additional details, see the LICENSE file
 
+using EOLib;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using XNAControls;
@@ -12,12 +13,16 @@ namespace EndlessClient.UIControls
     {
         protected Texture2D _displayPicture;
 
+        public Optional<Rectangle> SourceRectangle { get; set; }
+
         public PictureBox(Texture2D displayPicture, XNAControl parent = null)
             : base(null, null, parent)
         {
             SetNewPicture(displayPicture);
             if (parent == null)
                 Game.Components.Add(this);
+
+            SourceRectangle = new Optional<Rectangle>();
         }
 
         public void SetNewPicture(Texture2D displayPicture)
@@ -29,7 +34,11 @@ namespace EndlessClient.UIControls
         public override void Draw(GameTime gameTime)
         {
             SpriteBatch.Begin();
-            SpriteBatch.Draw(_displayPicture, DrawAreaWithOffset, Color.White);
+            SpriteBatch.Draw(
+                _displayPicture,
+                DrawAreaWithOffset,
+                SourceRectangle.HasValue ? SourceRectangle : null,
+                Color.White);
             SpriteBatch.End();
 
             base.Draw(gameTime);
