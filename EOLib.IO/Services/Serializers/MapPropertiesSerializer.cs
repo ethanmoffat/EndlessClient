7079427bc37214240b5c2f041d0a47d1ac88606a
Ethan Moffat <ethanmoffat@hotmail.com>
2016-09-29 14:12:32 -0600

    Rename fields in MapPropertiesSerializer

--- EOLib.IO/Services/Serializers/MapPropertiesSerializer.cs
+++ EOLib.IO/Services/Serializers/MapPropertiesSerializer.cs
@@ -12,14 +12,14 @@ namespace EOLib.IO.Services.Serializers
 {
     public class MapPropertiesSerializer : IMapSectionSerializer<IMapFileProperties>
     {
-        private readonly INumberEncoderService numberEncoderService;
-        private readonly IMapStringEncoderService mapStringEncoderService;
+        private readonly INumberEncoderService _numberEncoderService;
+        private readonly IMapStringEncoderService _mapStringEncoderService;
 
         public MapPropertiesSerializer(INumberEncoderService numberEncoderService,
                                        IMapStringEncoderService mapStringEncoderService)
         {
-            this.numberEncoderService = numberEncoderService;
-            this.mapStringEncoderService = mapStringEncoderService;
+            _numberEncoderService = numberEncoderService;
+            _mapStringEncoderService = mapStringEncoderService;
         }
 
         public byte[] SerializeToByteArray(IMapFileProperties mapEntity)
@@ -30,23 +30,23 @@ namespace EOLib.IO.Services.Serializers
             ret.AddRange(mapEntity.Checksum);
 
             var fullName = Enumerable.Repeat((byte)0xFF, 24).ToArray();
-            var encodedName = mapStringEncoderService.EncodeMapString(mapEntity.Name);
+            var encodedName = _mapStringEncoderService.EncodeMapString(mapEntity.Name);
             Array.Copy(encodedName, 0, fullName, fullName.Length - encodedName.Length, encodedName.Length);
             ret.AddRange(fullName);
 
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.PKAvailable ? 3 : 0, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber((byte)mapEntity.Effect, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.Music, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.MusicExtra, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.AmbientNoise, 2));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.Width, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.Height, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.FillTile, 2));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.MapAvailable ? 1 : 0, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.CanScroll ? 1 : 0, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.RelogX, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.RelogY, 1));
-            ret.AddRange(numberEncoderService.EncodeNumber(mapEntity.Unknown2, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.PKAvailable ? 3 : 0, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber((byte)mapEntity.Effect, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.Music, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.MusicExtra, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.AmbientNoise, 2));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.Width, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.Height, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.FillTile, 2));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.MapAvailable ? 1 : 0, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.CanScroll ? 1 : 0, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.RelogX, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.RelogY, 1));
+            ret.AddRange(_numberEncoderService.EncodeNumber(mapEntity.Unknown2, 1));
 
             return ret.ToArray();
         }
@@ -64,21 +64,21 @@ namespace EOLib.IO.Services.Serializers
             var checksumArray = data.Skip(7).Take(24).ToArray();
 
             properties = properties.WithChecksum(data.Skip(3).Take(4).ToArray())
-                .WithName(mapStringEncoderService.DecodeMapString(checksumArray))
-                .WithPKAvailable(numberEncoderService.DecodeNumber(data[31]) == 3 ||
+                .WithName(_mapStringEncoderService.DecodeMapString(checksumArray))
+                .WithPKAvailable(_numberEncoderService.DecodeNumber(data[31]) == 3 ||
                                  (checksumArray[0] == 0xFF && checksumArray[1] == 0x01))
-                .WithEffect((MapEffect) numberEncoderService.DecodeNumber(data[32]))
-                .WithMusic((byte) numberEncoderService.DecodeNumber(data[33]))
-                .WithMusicExtra((byte) numberEncoderService.DecodeNumber(data[34]))
-                .WithAmbientNoise((short) numberEncoderService.DecodeNumber(data[35], data[36]))
-                .WithWidth((byte) numberEncoderService.DecodeNumber(data[37]))
-                .WithHeight((byte) numberEncoderService.DecodeNumber(data[38]))
-                .WithFillTile((short) numberEncoderService.DecodeNumber(data[39], data[40]))
-                .WithMapAvailable(numberEncoderService.DecodeNumber(data[41]) == 1)
-                .WithScrollAvailable(numberEncoderService.DecodeNumber(data[42]) == 1)
-                .WithRelogX((byte) numberEncoderService.DecodeNumber(data[43]))
-                .WithRelogY((byte) numberEncoderService.DecodeNumber(data[44]))
-                .WithUnknown2((byte) numberEncoderService.DecodeNumber(data[45]));
+                .WithEffect((MapEffect) _numberEncoderService.DecodeNumber(data[32]))
+                .WithMusic((byte) _numberEncoderService.DecodeNumber(data[33]))
+                .WithMusicExtra((byte) _numberEncoderService.DecodeNumber(data[34]))
+                .WithAmbientNoise((short) _numberEncoderService.DecodeNumber(data[35], data[36]))
+                .WithWidth((byte) _numberEncoderService.DecodeNumber(data[37]))
+                .WithHeight((byte) _numberEncoderService.DecodeNumber(data[38]))
+                .WithFillTile((short) _numberEncoderService.DecodeNumber(data[39], data[40]))
+                .WithMapAvailable(_numberEncoderService.DecodeNumber(data[41]) == 1)
+                .WithScrollAvailable(_numberEncoderService.DecodeNumber(data[42]) == 1)
+                .WithRelogX((byte) _numberEncoderService.DecodeNumber(data[43]))
+                .WithRelogY((byte) _numberEncoderService.DecodeNumber(data[44]))
+                .WithUnknown2((byte) _numberEncoderService.DecodeNumber(data[45]));
 
             return properties;
         }
