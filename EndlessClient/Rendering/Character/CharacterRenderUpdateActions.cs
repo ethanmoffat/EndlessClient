d333ce70edadc49eae9fce7087b8d2702fe236e0
Ethan Moffat <ethanmoffat@hotmail.com>
2016-11-09 15:41:36 -0800

    Implement drawing for other characters

--- EndlessClient/Rendering/Character/CharacterRenderUpdateActions.cs
+++ EndlessClient/Rendering/Character/CharacterRenderUpdateActions.cs
@@ -3,7 +3,9 @@
 // For additional details, see the LICENSE file
 
 using EndlessClient.Rendering.Factories;
+using EOLib;
 using EOLib.Domain.Character;
+using EOLib.Domain.Map;
 using Microsoft.Xna.Framework;
 
 namespace EndlessClient.Rendering.Character
@@ -11,16 +13,19 @@ namespace EndlessClient.Rendering.Character
     public class CharacterRenderUpdateActions : ICharacterRenderUpdateActions
     {
         private readonly ICharacterProvider _characterProvider;
+        private readonly ICurrentMapStateProvider _currentMapStateProvider;
         private readonly ICharacterRendererFactory _characterRendererFactory;
         private readonly ICharacterRendererRepository _characterRendererRepository;
         private readonly ICharacterStateCache _characterStateCache;
 
         public CharacterRenderUpdateActions(ICharacterProvider characterProvider,
-            ICharacterRendererFactory characterRendererFactory,
-            ICharacterRendererRepository characterRendererRepository,
-            ICharacterStateCache characterStateCache)
+                                            ICurrentMapStateProvider currentMapStateProvider,
+                                            ICharacterRendererFactory characterRendererFactory,
+                                            ICharacterRendererRepository characterRendererRepository,
+                                            ICharacterStateCache characterStateCache)
         {
             _characterProvider = characterProvider;
+            _currentMapStateProvider = currentMapStateProvider;
             _characterRendererFactory = characterRendererFactory;
             _characterRendererRepository = characterRendererRepository;
             _characterStateCache = characterStateCache;
@@ -29,6 +34,7 @@ namespace EndlessClient.Rendering.Character
         public void UpdateCharacters(GameTime gameTime)
         {
             CacheMainCharacterRenderProperties();
+            CacheOtherCharacterRenderProperties();
             UpdateAllCharacters(gameTime);
         }
 
@@ -54,6 +60,41 @@ namespace EndlessClient.Rendering.Character
 
             _characterRendererRepository.MainCharacterRenderer.RenderProperties = actualProperties;
             _characterStateCache.UpdateMainCharacterState(actualProperties);
+        }
+
+        //todo: refactor to make this more easily readable and less branch-y
+        private void CacheOtherCharacterRenderProperties()
+        {
+            foreach (var character in _currentMapStateProvider.Characters)
+            {
+                var id = character.ID;
+
+                var actualProperties = character.RenderProperties;
+                var cachedProperties = _characterStateCache.HasCharacterWithID(id)
+                    ? new Optional<ICharacterRenderProperties>(_characterStateCache.CharacterRenderProperties[id])
+                    : Optional<ICharacterRenderProperties>.Empty;
+
+                if (!cachedProperties.HasValue)
+                {
+                    _characterStateCache.UpdateCharacterState(id, actualProperties);
+
+                    var renderer = _characterRendererFactory.CreateCharacterRenderer(actualProperties);
+
+                    //todo: figure out how to clean up existing renderers
+                    //if (_characterRendererRepository.CharacterRenderers.ContainsKey(id))
+                    //    _characterRendererRepository.CharacterRenderers[id].Dispose();
+                    _characterRendererRepository.CharacterRenderers.Add(id, renderer);
+                    _characterRendererRepository.CharacterRenderers[id].Initialize();
+
+                    continue;
+                }
+
+                if (cachedProperties.Value == actualProperties)
+                    continue;
+
+                _characterRendererRepository.CharacterRenderers[id].RenderProperties = actualProperties;
+                _characterStateCache.UpdateCharacterState(id, actualProperties);
+            }
         }
 
         private void UpdateAllCharacters(GameTime gameTime)
