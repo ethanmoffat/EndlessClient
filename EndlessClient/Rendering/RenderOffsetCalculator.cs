e30a3979ca3e6276ad79276bc458b5bdb4845c56
Ethan Moffat <ethanmoffat@hotmail.com>
2016-11-14 11:22:26 -0800

    Extract constants into meaningful names in RenderOffsetCalculator

--- EndlessClient/Rendering/RenderOffsetCalculator.cs
+++ EndlessClient/Rendering/RenderOffsetCalculator.cs
@@ -11,40 +11,45 @@ namespace EndlessClient.Rendering
 {
     public class RenderOffsetCalculator : IRenderOffsetCalculator
     {
+        private const int WidthFactor = 32;
+        private const int HeightFactor = 16;
+        private const int WalkWidthFactor = WidthFactor/4;
+        private const int WalkHeightFactor = HeightFactor/4;
+
         public int CalculateOffsetX(ICharacterRenderProperties properties)
         {
             var multiplier = properties.IsFacing(EODirection.Left, EODirection.Down) ? -1 : 1;
-            var walkAdjust = properties.IsActing(CharacterActionState.Walking) ? 8 * properties.WalkFrame : 0;
+            var walkAdjust = properties.IsActing(CharacterActionState.Walking) ? WalkWidthFactor * properties.WalkFrame : 0;
 
             //walkAdjust * multiplier is the old ViewAdjustX
-            return properties.MapX*32 - properties.MapY*32 + walkAdjust*multiplier;
+            return properties.MapX*WidthFactor - properties.MapY*WidthFactor + walkAdjust*multiplier;
         }
 
         public int CalculateOffsetY(ICharacterRenderProperties properties)
         {
             var multiplier = properties.IsFacing(EODirection.Left, EODirection.Up) ? -1 : 1;
-            var walkAdjust = properties.IsActing(CharacterActionState.Walking) ? 4 * properties.WalkFrame : 0;
+            var walkAdjust = properties.IsActing(CharacterActionState.Walking) ? WalkHeightFactor * properties.WalkFrame : 0;
 
             //walkAdjust * multiplier is the old ViewAdjustY
-            return properties.MapX*16 + properties.MapY*16 + walkAdjust*multiplier;
+            return properties.MapX*HeightFactor + properties.MapY*HeightFactor + walkAdjust*multiplier;
         }
 
         public int CalculateOffsetX(INPC npc)
         {
             var multiplier = npc.IsFacing(EODirection.Left, EODirection.Down) ? -1 : 1;
-            var walkAdjust = npc.IsActing(NPCActionState.Walking) ? 8 * npc.GetWalkFrame() : 0;
+            var walkAdjust = npc.IsActing(NPCActionState.Walking) ? WalkWidthFactor * npc.GetWalkFrame() : 0;
 
             //walkAdjust * multiplier is the old ViewAdjustX
-            return npc.X*32 - npc.Y*32 + walkAdjust*multiplier;
+            return npc.X*WidthFactor - npc.Y*WidthFactor + walkAdjust*multiplier;
         }
 
         public int CalculateOffsetY(INPC npc)
         {
             var multiplier = npc.IsFacing(EODirection.Left, EODirection.Down) ? -1 : 1;
-            var walkAdjust = npc.IsActing(NPCActionState.Walking) ? 4 * npc.GetWalkFrame() : 0;
+            var walkAdjust = npc.IsActing(NPCActionState.Walking) ? WalkHeightFactor * npc.GetWalkFrame() : 0;
 
             //walkAdjust * multiplier is the old ViewAdjustY
-            return npc.X*16 - npc.Y*16 + walkAdjust*multiplier;
+            return npc.X*HeightFactor - npc.Y*HeightFactor + walkAdjust*multiplier;
         }
     }
 
