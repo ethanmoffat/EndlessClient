6ea09503e3fc7e7cf3d6417b22a03bc754c357c1
Ethan Moffat <ethanmoffat@hotmail.com>
2016-11-14 16:04:58 -0800

    Add default configurable constants to logger

---
+++ EOLib.Logger/Constants.cs
@@ -0,0 +1,14 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+namespace EOLib.Logger
+{
+    internal static class Constants
+    {
+        internal const int DEFAULT_FLUSH_TIME_MS = 30000;
+        internal const int DEFAULT_FLUSH_LINES_WRITTEN = 50;
+        internal const int DEFAULT_SPLIT_FILE_BYTE_LENGTH = 100000;
+        internal const string DEFAULT_LOG_DIRECTORY = "Log";
+    }
+}
