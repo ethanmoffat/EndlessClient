c923dfbce5f5ff05177c0f6a71180598cb6b6b8b
Ethan Moffat <ethanmoffat@hotmail.com>
2017-02-12 18:02:03 -0800

    Issue #52: Rewrite SPStatusBar

--- EndlessClient/HUD/StatusBars/SPStatusBar.cs
+++ EndlessClient/HUD/StatusBars/SPStatusBar.cs
@@ -3,33 +3,37 @@
 // For additional details, see the LICENSE file
 
 using System;
+using EOLib.Domain.Character;
+using EOLib.Graphics;
 using Microsoft.Xna.Framework;
 
 namespace EndlessClient.HUD.StatusBars
 {
-    public class SPStatusBar : BaseStatusBar
+    public class SPStatusBar : StatusBarBase
     {
-        public SPStatusBar()
+        public SPStatusBar(INativeGraphicsManager nativeGraphicsManager,
+                           ICharacterProvider characterProvider)
+            : base(nativeGraphicsManager, characterProvider)
         {
-            DrawLocation = new Vector2(320, 0);
-            drawArea = new Rectangle((int)DrawLocation.X, (int)DrawLocation.Y, m_elemSourceRect.Width, m_elemSourceRect.Height);
-            m_elemSourceRect.Offset(m_elemSourceRect.Width * 2, 0);
+            DrawArea = new Rectangle(320, 0, _sourceRectangleArea.Width, _sourceRectangleArea.Height);
+            _sourceRectangleArea.Offset(_sourceRectangleArea.Width * 2, _sourceRectangleArea.Height);
         }
 
         protected override void UpdateLabelText()
         {
-            m_label.Text = $"{m_stats.SP}/{m_stats.MaxSP}";
+            _label.Text = $"{Stats[CharacterStat.SP]}/{Stats[CharacterStat.MaxSP]}";
         }
 
         protected override void DrawStatusBar()
         {
-            int srcWidth = 25 + (int)Math.Round((m_stats.SP / (double)m_stats.MaxSP) * 79);
-            Rectangle maskSrc = new Rectangle(m_elemSourceRect.X, m_elemSourceRect.Height, srcWidth, m_elemSourceRect.Height);
+            //todo: figure out these magic numbers
+            var srcWidth = 25 + (int)Math.Round(Stats[CharacterStat.SP] / (double)Stats[CharacterStat.MaxSP] * 79);
+            var maskSrc = new Rectangle(_sourceRectangleArea.X, _sourceRectangleArea.Y, srcWidth, _sourceRectangleArea.Height);
 
-            SpriteBatch.Begin();
-            SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), m_elemSourceRect, Color.White);
-            SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), maskSrc, Color.White);
-            SpriteBatch.End();
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_texture, DrawPositionWithParentOffset, _sourceRectangleArea, Color.White);
+            _spriteBatch.Draw(_texture, DrawPositionWithParentOffset, maskSrc, Color.White);
+            _spriteBatch.End();
         }
     }
 }
