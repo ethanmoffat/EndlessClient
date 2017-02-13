e132957eebc11e798f3b0dcdedab11e569a53938
Ethan Moffat <ethanmoffat@hotmail.com>
2017-02-12 18:08:12 -0800

    Issue #52: Rewrite TNLStatusBar

--- EndlessClient/HUD/StatusBars/TNLStatusBar.cs
+++ EndlessClient/HUD/StatusBars/TNLStatusBar.cs
@@ -3,36 +3,50 @@
 // For additional details, see the LICENSE file
 
 using System;
-using EndlessClient.Old;
+using System.Collections.Generic;
+using EOLib.Domain.Character;
+using EOLib.Graphics;
 using Microsoft.Xna.Framework;
 
 namespace EndlessClient.HUD.StatusBars
 {
-    public class TNLStatusBar : BaseStatusBar
+    public class TNLStatusBar : StatusBarBase
     {
-        public TNLStatusBar()
+        private readonly IExperienceTableProvider _experienceTableProvider;
+
+        public TNLStatusBar(INativeGraphicsManager nativeGraphicsManager,
+                            ICharacterProvider characterProvider,
+                            IExperienceTableProvider experienceTableProvider)
+            : base(nativeGraphicsManager, characterProvider)
         {
-            DrawLocation = new Vector2(430, 0);
-            drawArea = new Rectangle((int)DrawLocation.X, (int)DrawLocation.Y, m_elemSourceRect.Width, m_elemSourceRect.Height);
-            m_elemSourceRect = new Rectangle(m_elemSourceRect.Width * 3 - 1, 0, m_elemSourceRect.Width + 1, m_elemSourceRect.Height);
+            _experienceTableProvider = experienceTableProvider;
+            DrawArea = new Rectangle(430, 0, _sourceRectangleArea.Width, _sourceRectangleArea.Height);
+
+            _sourceRectangleArea = new Rectangle(_sourceRectangleArea.Width*3 - 1,
+                                                 _sourceRectangleArea.Height,
+                                                 _sourceRectangleArea.Width + 1,
+                                                 _sourceRectangleArea.Height);
         }
 
         protected override void UpdateLabelText()
         {
-            m_label.Text = $"{OldWorld.Instance.exp_table[m_stats.Level + 1] - m_stats.Experience}";
+            _label.Text = $"{ExpTable[Stats[CharacterStat.Level] + 1] - Stats[CharacterStat.Experience]}";
         }
 
         protected override void DrawStatusBar()
         {
-            int thisLevel = OldWorld.Instance.exp_table[m_stats.Level];
-            int nextLevel = OldWorld.Instance.exp_table[m_stats.Level + 1];
-            int srcWidth = 25 + (int)Math.Round(((m_stats.Experience - thisLevel) / (double)(nextLevel - thisLevel)) * 79);
-            Rectangle maskSrc = new Rectangle(m_elemSourceRect.X, m_elemSourceRect.Height, srcWidth, m_elemSourceRect.Height);
+            //todo: figure out these magic numbers
+            var thisLevelExp = ExpTable[Stats[CharacterStat.Level]];
+            var nextLevelExp = ExpTable[Stats[CharacterStat.Level] + 1];
+            var srcWidth = 25 + (int)Math.Round((Stats[CharacterStat.Experience] - thisLevelExp) / (double)(nextLevelExp - thisLevelExp) * 79);
+            var maskSrc = new Rectangle(_sourceRectangleArea.X, _sourceRectangleArea.Y, srcWidth, _sourceRectangleArea.Height);
 
-            SpriteBatch.Begin();
-            SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), m_elemSourceRect, Color.White);
-            SpriteBatch.Draw(m_textSheet, new Vector2(DrawAreaWithOffset.X, DrawAreaWithOffset.Y), maskSrc, Color.White);
-            SpriteBatch.End();
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_texture, DrawPositionWithParentOffset, _sourceRectangleArea, Color.White);
+            _spriteBatch.Draw(_texture, DrawPositionWithParentOffset, maskSrc, Color.White);
+            _spriteBatch.End();
         }
+
+        private IReadOnlyList<int> ExpTable => _experienceTableProvider.ExperienceByLevel;
     }
 }
