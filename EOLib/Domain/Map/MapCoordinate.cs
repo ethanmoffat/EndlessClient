2c1d907991746c954978eeac66df1392360c4f2c
Ethan Moffat <ethanmoffat@hotmail.com>
2019-07-03 23:02:21 -0700

    Improve performance of check for an NPC renderer on grid coordinates

---
+++ EOLib/Domain/Map/MapCoordinate.cs
@@ -0,0 +1,35 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2019
+
+namespace EOLib.Domain.Map
+{
+    public struct MapCoordinate
+    {
+        public int X { get; }
+
+        public int Y { get; }
+
+        public MapCoordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"{X}, {Y}";
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is MapCoordinate))
+                return false;
+
+            var other = (MapCoordinate) obj;
+            return X == other.X && Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = 397 ^ X.GetHashCode();
+            hash = (hash * 397) ^ Y.GetHashCode();
+            return hash;
+        }
+    }
+}
