674f5610de8675ca4acd1b10fd3ca57f9e25cf2d
Ethan Moffat <ethanmoffat@hotmail.com>
2020-01-22 21:50:50 -0800

    Cross-platform HDSerialNumberService

---
+++ EOLib/HDSerialNumberServiceLinux.cs
@@ -0,0 +1,19 @@
+﻿// Original Work Copyright (c) Ethan Moffat 2014-2016
+// This file is subject to the GPL v2 License
+// For additional details, see the LICENSE file
+
+using AutomaticTypeMapper;
+
+namespace EOLib
+{
+#if LINUX
+    [AutoMappedType]
+    public class HDSerialNumberServiceLinux : IHDSerialNumberService
+    {
+        public string GetHDSerialNumber()
+        {
+            return "12345"; // Just like my luggage
+        }
+    }
+#endif
+}
