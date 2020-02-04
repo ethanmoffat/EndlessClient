9f04e58e657d16360ff03e635ec945ea9e63ebf4
Ethan Moffat <ethanmoffat@hotmail.com>
2020-02-03 22:55:46 -0800

    Align character skin for ranged attacks

--- EndlessClient/Rendering/CharacterProperties/SkinRenderLocationCalculator.cs
+++ EndlessClient/Rendering/CharacterProperties/SkinRenderLocationCalculator.cs
@@ -21,17 +21,47 @@ namespace EndlessClient.Rendering.CharacterProperties
 
         public Vector2 CalculateDrawLocationOfCharacterSkin(Rectangle skinRectangle, Rectangle parentCharacterDrawArea)
         {
-            var resX = -(float)Math.Floor(Math.Abs((float)skinRectangle.Width - parentCharacterDrawArea.Width) / 2);
-            var resY = -(float)Math.Floor(Math.Abs((float)skinRectangle.Height - parentCharacterDrawArea.Height) / 2);
+            float resX, resY;
 
-            if (_renderProperties.IsActing(CharacterActionState.SpellCast))
-                resY -= 2;
+            // Ranged weapon attack frame is offset based on the Left or Right border of the parent character draw area
+            // Ranged weapon graphics do not use the centering approach
+            if (_renderProperties.IsRangedWeapon && _renderProperties.AttackFrame == 1)
+            {
+                var isFlipped = _renderProperties.IsFacing(EODirection.Right, EODirection.Up);
+                var needsExtraOffset = _renderProperties.IsFacing(EODirection.Right, EODirection.Down);
 
-            // This specific frame is a bitch
-            if (_renderProperties.Gender == 1 && _renderProperties.AttackFrame == 1)
-                resX += _renderProperties.IsFacing(EODirection.Up, EODirection.Right) ? 2 : -2;
+                var startPixelX = isFlipped ? parentCharacterDrawArea.Left : parentCharacterDrawArea.Right;
+                var offsetFactor = isFlipped ? 0 : -1;
 
-            return new Vector2(parentCharacterDrawArea.X + resX, parentCharacterDrawArea.Y + resY);
+                resX = startPixelX + ((needsExtraOffset ? 2 : 0) + skinRectangle.Width) * offsetFactor;
+
+                // essentially, is it facing EODirection.Right?
+                if (isFlipped && needsExtraOffset)
+                    resX += 2;
+
+                // male needs an extra +/- 2
+                if (_renderProperties.Gender == 1)
+                    resX += 2 * (isFlipped ? 1 : -1);
+
+                resY = parentCharacterDrawArea.Y + (needsExtraOffset ? 1 : 0);
+            }
+            else
+            {
+                resX = -(float)Math.Floor(Math.Abs((float)skinRectangle.Width - parentCharacterDrawArea.Width) / 2);
+                resY = -(float)Math.Floor(Math.Abs((float)skinRectangle.Height - parentCharacterDrawArea.Height) / 2);
+
+                if (_renderProperties.IsActing(CharacterActionState.SpellCast))
+                    resY -= 2;
+
+                // This specific frame is a bitch
+                if (_renderProperties.Gender == 1 && _renderProperties.AttackFrame == 1)
+                    resX += _renderProperties.IsFacing(EODirection.Up, EODirection.Right) ? 2 : -2;
+
+                resX += parentCharacterDrawArea.X;
+                resY += parentCharacterDrawArea.Y;
+            }
+
+            return new Vector2(resX, resY);
         }
     }
 }
