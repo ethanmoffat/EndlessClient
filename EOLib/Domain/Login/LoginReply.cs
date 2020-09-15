196c44783b4ebfb7de2859a7326d8d6e85198f38
Ethan Moffat <ethanmoffat@hotmail.com>
2020-09-14 18:35:22 -0700

    Handle LoginReply for ban

--- EOLib/Domain/Login/LoginReply.cs
+++ EOLib/Domain/Login/LoginReply.cs
@@ -5,6 +5,7 @@
         WrongUser = 1,
         WrongUserPass = 2,
         Ok = 3,
+        AccountBanned = 4,
         LoggedIn = 5,
         Busy = 6,
         THIS_IS_WRONG = 255
--- EndlessClient/Dialogs/Actions/ErrorDialogDisplayAction.cs
+++ EndlessClient/Dialogs/Actions/ErrorDialogDisplayAction.cs
@@ -119,6 +119,7 @@ namespace EndlessClient.Dialogs.Actions
             {
                 case LoginReply.WrongUser: message = DialogResourceID.LOGIN_ACCOUNT_NAME_NOT_FOUND; break;
                 case LoginReply.WrongUserPass: message = DialogResourceID.LOGIN_ACCOUNT_NAME_OR_PASSWORD_NOT_FOUND; break;
+                case LoginReply.AccountBanned: message = DialogResourceID.LOGIN_BANNED_FROM_SERVER; break;
                 case LoginReply.LoggedIn: message = DialogResourceID.LOGIN_ACCOUNT_ALREADY_LOGGED_ON; break;
                 case LoginReply.Busy: message = DialogResourceID.CONNECTION_SERVER_IS_FULL;  break;
                 default: throw new ArgumentOutOfRangeException(nameof(loginError), loginError, null);
