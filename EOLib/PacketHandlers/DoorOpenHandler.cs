ab3c7bb94af9ffb92c8064e41ab758ad7f9a440c
Ethan Moffat <ethan@moffat.io>
2021-01-19 20:08:30 -0800

    Fix handling for other players opening doors

--- EOLib/PacketHandlers/DoorOpenHandler.cs
+++ EOLib/PacketHandlers/DoorOpenHandler.cs
@@ -11,16 +11,19 @@ namespace EOLib.PacketHandlers
     public class DoorOpenHandler : InGameOnlyPacketHandler
     {
         private readonly ICurrentMapStateRepository _currentMapStateRepository;
+        private readonly ICurrentMapProvider _currentMapProvider;
 
         public override PacketFamily Family => PacketFamily.Door;
 
         public override PacketAction Action => PacketAction.Open;
 
         public DoorOpenHandler(IPlayerInfoProvider playerInfoProvider,
-                               ICurrentMapStateRepository currentMapStateRepository)
+                               ICurrentMapStateRepository currentMapStateRepository,
+                               ICurrentMapProvider currentMapProvider)
             : base(playerInfoProvider)
         {
             _currentMapStateRepository = currentMapStateRepository;
+            _currentMapProvider = currentMapProvider;
         }
 
         public override bool HandlePacket(IPacket packet)
@@ -33,7 +36,14 @@ namespace EOLib.PacketHandlers
             {
                 warp = _currentMapStateRepository.PendingDoors.Single(w => w.X == x && w.Y == y);
             }
-            catch { return false; }
+            catch
+            {
+                // another player attempted to open a door
+                warp = new Warp(_currentMapProvider.CurrentMap.Warps[y, x]);
+
+                if (warp.X < 0 && warp.Y < 0)
+                    return false; // bad packet? this should never be hit
+            }
 
             _currentMapStateRepository.PendingDoors.Remove(warp);
             _currentMapStateRepository.OpenDoors.Add(warp);
