a4748cd92b410b0a667e564fde5f2201b9e42c34
Ethan Moffat <ethan@moffat.io>
2021-05-09 13:57:25 -0700

    Make code in TrainerBot easier to read; add retries for NoDataSentException

--- EOBot/TrainerBot.cs
+++ EOBot/TrainerBot.cs
@@ -5,9 +5,12 @@ using EOLib.Domain.Extensions;
 using EOLib.Domain.Login;
 using EOLib.Domain.Map;
 using EOLib.IO;
+using EOLib.IO.Pub;
 using EOLib.IO.Repositories;
+using EOLib.Net;
 using EOLib.Net.Handlers;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,6 +27,15 @@ namespace EOBot
         private readonly string _account;
         private readonly string _password;
         private readonly string _character;
+
+        private ICharacterActions _characterActions;
+        private IMapActions _mapActions;
+
+        private ICharacterRepository _characterRepository;
+
+        private IPubFile<EIFRecord> _itemData;
+        private IPubFile<ENFRecord> _npcData;
+        private IPubFile<ESFRecord> _spellData;
 
         public TrainerBot(int botIndex, string account, string password, string character)
             : base(botIndex)
@@ -53,23 +65,19 @@ namespace EOBot
             await helper.LoginToCharacterAsync(_character);
 
             var c = DependencyMaster.TypeRegistry[_index];
-            var mapActions = c.Resolve<IMapActions>();
-            var charRepo = c.Resolve<ICharacterRepository>();
-            var charActions = c.Resolve<ICharacterActions>();
+
+            _mapActions = c.Resolve<IMapActions>();
+            _characterRepository = c.Resolve<ICharacterRepository>();
+            _characterActions = c.Resolve<ICharacterActions>();
             var mapCellStateProvider = c.Resolve<IMapCellStateProvider>();
             var handler = c.Resolve<IOutOfBandPacketHandler>();
-            var itemData = c.Resolve<IEIFFileProvider>().EIFFile;
-            var npcData = c.Resolve<IENFFileProvider>().ENFFile;
-            var spellData = c.Resolve<IESFFileProvider>().ESFFile;
+            _itemData = c.Resolve<IEIFFileProvider>().EIFFile;
+            _npcData = c.Resolve<IENFFileProvider>().ENFFile;
+            _spellData = c.Resolve<IESFFileProvider>().ESFFile;
             var charInventoryRepo = c.Resolve<ICharacterInventoryRepository>();
 
-            var healItems = charInventoryRepo.ItemInventory
-                .Where(x => itemData.Data.Any(y => y.ID == x.ItemID && y.Type == ItemType.Heal))
-                .ToList();
-
-            var healSpells = charInventoryRepo.SpellInventory
-                .Where(x => spellData.Data.Any(y => y.ID == x.ID && y.Type == SpellType.Heal))
-                .ToList();
+            var healItems = new List<IInventoryItem>();
+            var healSpells = new List<IInventorySpell>();
 
             int attackCount = 0;
             bool time_to_die = false;
@@ -78,133 +86,178 @@ namespace EOBot
             {
                 handler.PollForPacketsAndHandle();
 
-                var charRenderProps = charRepo.MainCharacter.RenderProperties;
+                var character = _characterRepository.MainCharacter;
+                var charRenderProps = character.RenderProperties;
+
                 var nextX = charRenderProps.GetDestinationX();
                 var nextY = charRenderProps.GetDestinationY();
                 var currentCellState = mapCellStateProvider.GetCellStateAt(charRenderProps.MapX, charRenderProps.MapY);
                 var cellState = mapCellStateProvider.GetCellStateAt(nextX, nextY);
 
-                if (time_to_die)
+                if (!time_to_die)
                 {
-                    await Task.Delay(TimeSpan.FromSeconds(1.0 / 30.0));
-                    continue;
-                }
-
-                if ((attackCount < CONSECUTIVE_ATTACK_COUNT && !currentCellState.NPC.HasValue) || cellState.NPC.HasValue)
-                {
-                    if (cellState.NPC.HasValue && charRepo.MainCharacter.Stats[CharacterStat.HP] > charRepo.MainCharacter.Stats[CharacterStat.MaxHP] * .1)
+                    if ((attackCount < CONSECUTIVE_ATTACK_COUNT && !currentCellState.NPC.HasValue) || cellState.NPC.HasValue)
                     {
-                        Console.WriteLine($"[ATTK] {cellState.NPC.Value.Index,7} - {npcData.Data.Single(x => x.ID == cellState.NPC.Value.ID).Name}");
-                        charActions.Attack();
-                        attackCount++;
-                    }
-                    else if (cellState.Items.Any())
-                    {
-                        foreach (var item in cellState.Items)
+                        if (cellState.NPC.HasValue && character.Stats[CharacterStat.HP] > character.Stats[CharacterStat.MaxHP] * .1)
                         {
-                            Console.WriteLine($"[TAKE] {item.Amount,7} - {itemData.Data.Single(x => x.ID == item.ItemID).Name}");
-                            mapActions.PickUpItem(item);
+                            await Attack(cellState);
+                            attackCount++;
                         }
-                    }
-                    else if (healSpells.Any() && charRepo.MainCharacter.Stats[CharacterStat.HP] < charRepo.MainCharacter.Stats[CharacterStat.MaxHP]
-                        && charRepo.MainCharacter.Stats[CharacterStat.TP] > charRepo.MainCharacter.Stats[CharacterStat.MaxTP] * .5)
-                    {
-                        var spellToUse = spellData.Data
-                            .Where(x => healSpells.Any(y => y.ID == x.ID) && x.Target != SpellTarget.Group)
-                            .OrderByDescending(x => x.HP)
-                            .First();
-
-                        Console.WriteLine($"[CAST] {spellToUse.HP,4} HP - {spellToUse.Name}");
-
-                        charActions.PrepareCastSpell(spellToUse.ID);
-                        await Task.Delay((int)Math.Round(spellToUse.CastTime / 2.0 * 950)); // ?
-
-                        charActions.CastSpell(spellToUse.ID, charRepo.MainCharacter);
-                        await Task.Delay((int)Math.Round(spellToUse.CastTime / 2.0 * 950)); // ?
-                    }
-                    else if (healItems.Any() && charRepo.MainCharacter.Stats[CharacterStat.HP] < charRepo.MainCharacter.Stats[CharacterStat.MaxHP] * .3)
-                    {
-                        while (charRepo.MainCharacter.Stats[CharacterStat.HP] < charRepo.MainCharacter.Stats[CharacterStat.MaxHP] * .6)
+                        else if (cellState.Items.Any())
                         {
-                            var itemToUse = itemData.Data
-                                .Where(x => healItems.Any(y => y.ItemID == x.ID))
-                                .OrderBy(x => x.HP)
-                                .First();
-                            var amount = healItems.Single(x => x.ItemID == itemToUse.ID).Amount;
-
-                            Console.WriteLine($"[USE ] {itemToUse.Name} - {itemToUse.HP} HP - inventory: {amount - 1} - (other heal item types: {healItems.Count - 1})");
-
-                            charActions.UseItem(itemToUse.ID);
-                            await Task.Delay(ATTACK_BACKOFF_MS);
+                            await PickUpItems(cellState);
                         }
+                        else if (healSpells.Any() && character.Stats[CharacterStat.HP] < character.Stats[CharacterStat.MaxHP]
+                            && character.Stats[CharacterStat.TP] > character.Stats[CharacterStat.MaxTP] * .5)
+                        {
+                            await CastHealSpell(healSpells);
+                        }
+                        else if (healItems.Any() && character.Stats[CharacterStat.HP] < character.Stats[CharacterStat.MaxHP] * .3)
+                        {
+                            await UseHealItem(healItems, targetHealthPercent: .6);
+                        }
+                        else if (character.Stats[CharacterStat.Weight] >= character.Stats[CharacterStat.MaxWeight])
+                        {
+                            await SitDown();
+                            time_to_die = true;
+                        }
+
+                        healItems = charInventoryRepo.ItemInventory
+                            .Where(x => _itemData.Data.Any(y => y.ID == x.ItemID && y.Type == ItemType.Heal))
+                            .ToList();
+
+                        healSpells = charInventoryRepo.SpellInventory
+                            .Where(x => _spellData.Data.Any(y => y.ID == x.ID && y.Type == SpellType.Heal))
+                            .ToList();
                     }
-                    else if (charRepo.MainCharacter.Stats[CharacterStat.Weight] >= charRepo.MainCharacter.Stats[CharacterStat.MaxWeight])
-                    {
-                        Console.WriteLine($"[SIT ] OVER WEIGHT LIMIT - TIME TO DIE");
-
-                        charActions.ToggleSit();
-                        time_to_die = true;
-                    }
-
-                    await Task.Delay(ATTACK_BACKOFF_MS);
-
-                    healItems = charInventoryRepo.ItemInventory
-                        .Where(x => itemData.Data.Any(y => y.ID == x.ItemID && y.Type == ItemType.Heal))
-                        .ToList();
-
-                    healSpells = charInventoryRepo.SpellInventory
-                        .Where(x => spellData.Data.Any(y => y.ID == x.ID && y.Type == SpellType.Heal))
-                        .ToList();
-                }
-                else
-                {
-                    if (!currentCellState.NPC.HasValue)
-                        Console.WriteLine($"[MOVE] Walking due to consecutive attacks: {attackCount}");
                     else
-                        Console.WriteLine($"[ATTK] Killing NPC at player location");
-
-                    await Task.Delay(TimeSpan.FromMilliseconds(300));
-
-                    var direction = charRenderProps.Direction;
-                    charActions.Walk();
-                    await Task.Delay(TimeSpan.FromMilliseconds(WALK_BACKOFF_MS));
-
-                    charActions.Face(direction.Opposite());
-                    // todo: character actions Face() should also change the character's direction instead of relying on client to update it separately
-                    charRepo.MainCharacter = charRepo.MainCharacter.WithRenderProperties(charRepo.MainCharacter.RenderProperties.WithDirection(direction.Opposite()));
-                    await Task.Delay(TimeSpan.FromMilliseconds(FACE_BACKOFF_MS));
-
-                    // kill NPC if it was in our starting point
-                    while (currentCellState.NPC.HasValue && !TerminationRequested)
                     {
-                        Console.WriteLine($"[ATTK] {currentCellState.NPC.Value.Index,7} : {npcData.Data.Single(x => x.ID == currentCellState.NPC.Value.ID).Name}");
-                        charActions.Attack();
-                        await Task.Delay(TimeSpan.FromMilliseconds(ATTACK_BACKOFF_MS));
+                        if (!currentCellState.NPC.HasValue)
+                            Console.WriteLine($"[MOVE] Walking due to consecutive attacks: {attackCount}");
+                        else
+                            Console.WriteLine($"[ATTK] Killing NPC at player location");
 
-                        handler.PollForPacketsAndHandle();
-                        currentCellState = mapCellStateProvider.GetCellStateAt(charRenderProps.MapX, charRenderProps.MapY);
-                    }
+                        var originalDirection = charRenderProps.Direction;
+                        await Walk();
+                        await Face(originalDirection.Opposite());
 
-                    if (currentCellState.Items.Any())
-                    {
-                        foreach (var item in currentCellState.Items)
+                        // kill NPC if it was in our starting point
+                        while (currentCellState.NPC.HasValue && !TerminationRequested)
                         {
-                            Console.WriteLine($"[TAKE] {item.Amount,7} - {itemData.Data.Single(x => x.ID == item.ItemID).Name}");
-                            mapActions.PickUpItem(item);
+                            await Attack(currentCellState);
+
+                            handler.PollForPacketsAndHandle();
+                            currentCellState = mapCellStateProvider.GetCellStateAt(charRenderProps.MapX, charRenderProps.MapY);
                         }
+
+                        await PickUpItems(currentCellState);
+
+                        await Walk();
+                        await Face(originalDirection);
+
+                        attackCount = 0;
                     }
-
-                    charActions.Walk();
-                    await Task.Delay(TimeSpan.FromMilliseconds(WALK_BACKOFF_MS));
-
-                    charActions.Face(direction);
-                    charRepo.MainCharacter = charRepo.MainCharacter.WithRenderProperties(charRepo.MainCharacter.RenderProperties.WithDirection(direction));
-                    await Task.Delay(TimeSpan.FromMilliseconds(FACE_BACKOFF_MS));
-
-                    attackCount = 0;
                 }
 
-                await Task.Delay(TimeSpan.FromSeconds(1.0 / 30.0));
+                await Task.Delay(TimeSpan.FromSeconds(1.0 / 8.0));
+            }
+        }
+
+        private async Task Attack(IMapCellState cellState)
+        {
+            Console.WriteLine($"[ATTK] {cellState.NPC.Value.Index,7} - {_npcData.Data.Single(x => x.ID == cellState.NPC.Value.ID).Name}");
+            await TrySend(_characterActions.Attack);
+            await Task.Delay(TimeSpan.FromMilliseconds(ATTACK_BACKOFF_MS));
+        }
+
+        private async Task Walk()
+        {
+            var renderProps = _characterRepository.MainCharacter.RenderProperties;
+            Console.WriteLine($"[WALK] {renderProps.GetDestinationX(),3},{renderProps.GetDestinationY(),3}");
+            await TrySend(_characterActions.Walk);
+            await Task.Delay(TimeSpan.FromMilliseconds(WALK_BACKOFF_MS));
+        }
+
+        private async Task Face(EODirection direction)
+        {
+            Console.WriteLine($"[FACE] {Enum.GetName(typeof(EODirection), direction),7}");
+            await TrySend(() => _characterActions.Face(direction.Opposite()));
+
+            // todo: character actions Face() should also change the character's direction instead of relying on client to update it separately
+            _characterRepository.MainCharacter = _characterRepository.MainCharacter
+                .WithRenderProperties(_characterRepository.MainCharacter.RenderProperties.WithDirection(direction.Opposite()));
+
+            await Task.Delay(TimeSpan.FromMilliseconds(FACE_BACKOFF_MS));
+        }
+
+        private async Task PickUpItems(IMapCellState cellState)
+        {
+            foreach (var item in cellState.Items)
+            {
+                Console.WriteLine($"[TAKE] {item.Amount,7} - {_itemData.Data.Single(x => x.ID == item.ItemID).Name}");
+                await TrySend(() => _mapActions.PickUpItem(item));
+
+                await Task.Delay(TimeSpan.FromMilliseconds(100));
+            }
+        }
+
+        private async Task CastHealSpell(IEnumerable<IInventorySpell> healSpells)
+        {
+            var spellToUse = _spellData.Data
+                .Where(x => healSpells.Any(y => y.ID == x.ID) && x.Target != SpellTarget.Group)
+                .OrderByDescending(x => x.HP)
+                .First();
+
+            Console.WriteLine($"[CAST] {spellToUse.HP,4} HP - {spellToUse.Name}");
+
+            await TrySend(() => _characterActions.PrepareCastSpell(spellToUse.ID));
+            await Task.Delay((int)Math.Round(spellToUse.CastTime / 2.0 * 950)); // ?
+
+            await TrySend(() => _characterActions.CastSpell(spellToUse.ID, _characterRepository.MainCharacter));
+            await Task.Delay((int)Math.Round(spellToUse.CastTime / 2.0 * 950)); // ?
+        }
+
+        private async Task UseHealItem(IEnumerable<IInventoryItem> healItems, double targetHealthPercent)
+        {
+            while (_characterRepository.MainCharacter.Stats[CharacterStat.HP] < _characterRepository.MainCharacter.Stats[CharacterStat.MaxHP] * targetHealthPercent)
+            {
+                var itemToUse = _itemData.Data
+                    .Where(x => healItems.Any(y => y.ItemID == x.ID))
+                    .OrderBy(x => x.HP)
+                    .First();
+                var amount = healItems.Single(x => x.ItemID == itemToUse.ID).Amount;
+
+                Console.WriteLine($"[USE ] {itemToUse.Name} - {itemToUse.HP} HP - inventory: {amount - 1} - (other heal item types: {healItems.Count() - 1})");
+
+                await TrySend(() => _characterActions.UseItem(itemToUse.ID));
+
+                await Task.Delay(ATTACK_BACKOFF_MS);
+            }
+        }
+
+        private async Task SitDown()
+        {
+            Console.WriteLine($"[SIT ] OVER WEIGHT LIMIT - TIME TO DIE");
+            await TrySend(_characterActions.ToggleSit);
+        }
+
+        private async Task TrySend(Action action, int attempts = 3)
+        {
+            for (int i = 1; i <= attempts; i++)
+            {
+                try
+                {
+                    action();
+                    break;
+                }
+                catch (NoDataSentException)
+                {
+                    Console.WriteLine($"[EX  ] {i} / {attempts}   - No data sent");
+                    if (i == attempts)
+                        throw;
+
+                    await Task.Delay(TimeSpan.FromSeconds(i * i));
+                }
             }
         }
     }
