1b092e297d8d33e0767997afdf9d3b74ff732894
Ethan Moffat <ethan@moffat.io>
2022-02-08 16:43:28 -0800

    Convert BatchPub/PacketDecoder to sdk style projects Remove Linux build configurations from solution

--- PacketDecoder/Program.cs
+++ PacketDecoder/Program.cs
@@ -12,6 +12,7 @@ namespace PacketDecoder
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
