6cebd7b2047ad01150906723b0d97800a258b1ed
Ethan Moffat <ethan@moffat.io>
2022-03-28 22:47:28 -0700

    Add ItemTransferDialog and factory

---
+++ EndlessClient/Dialogs/Factories/ItemTransferDialogFactory.cs
@@ -0,0 +1,50 @@
+﻿using AutomaticTypeMapper;
+using EndlessClient.Content;
+using EndlessClient.Dialogs.Services;
+using EndlessClient.Input;
+using EOLib.Graphics;
+using EOLib.Localization;
+
+namespace EndlessClient.Dialogs.Factories
+{
+    [AutoMappedType]
+    public class ItemTransferDialogFactory : IItemTransferDialogFactory
+    {
+        private readonly INativeGraphicsManager _nativeGraphicsManager;
+        private readonly IEODialogButtonService _eoDialogButtonService;
+        private readonly ILocalizedStringFinder _localizedStringFinder;
+        private readonly IContentProvider _contentProvider;
+        private readonly IKeyboardDispatcherRepository _keyboardDispatcherRepository;
+
+        public ItemTransferDialogFactory(INativeGraphicsManager nativeGraphicsManager,
+                                         IEODialogButtonService eoDialogButtonService,
+                                         ILocalizedStringFinder localizedStringFinder,
+                                         IContentProvider contentProvider,
+                                         IKeyboardDispatcherRepository keyboardDispatcherRepository)
+        {
+            _nativeGraphicsManager = nativeGraphicsManager;
+            _eoDialogButtonService = eoDialogButtonService;
+            _localizedStringFinder = localizedStringFinder;
+            _contentProvider = contentProvider;
+            _keyboardDispatcherRepository = keyboardDispatcherRepository;
+        }
+
+        public ItemTransferDialog CreateItemTransferDialog(string itemName, ItemTransferDialog.TransferType transferType, int totalAmount, EOResourceID message)
+        {
+            return new ItemTransferDialog(_nativeGraphicsManager,
+                _eoDialogButtonService,
+                _localizedStringFinder,
+                _contentProvider,
+                _keyboardDispatcherRepository,
+                itemName,
+                transferType,
+                totalAmount,
+                message);
+        }
+    }
+
+    public interface IItemTransferDialogFactory
+    {
+        ItemTransferDialog CreateItemTransferDialog(string itemName, ItemTransferDialog.TransferType transferType, int totalAmount, EOResourceID message);
+    }
+}
