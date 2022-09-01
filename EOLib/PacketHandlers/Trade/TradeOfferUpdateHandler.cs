33bd06be45e921b9d2cd8315e6622936e063e3c3
Ethan Moffat <ethan@moffat.io>
2022-08-31 20:17:50 -0700

    Calculate inventory weight changes when completing a trade

--- EOLib/PacketHandlers/Trade/TradeOfferUpdateHandler.cs
+++ EOLib/PacketHandlers/Trade/TradeOfferUpdateHandler.cs
@@ -3,6 +3,7 @@ using EOLib.Domain.Character;
 using EOLib.Domain.Login;
 using EOLib.Domain.Notifiers;
 using EOLib.Domain.Trade;
+using EOLib.IO.Repositories;
 using EOLib.Net;
 using EOLib.Net.Handlers;
 using Optional;
@@ -78,21 +79,24 @@ namespace EOLib.PacketHandlers.Trade
     /// </summary>
     public class TradeUseHandler : TradeOfferUpdateHandler
     {
-        private readonly ICharacterProvider _characterProvider;
+        private readonly ICharacterRepository _characterRepository;
         private readonly ICharacterInventoryRepository _characterInventoryRepository;
+        private readonly IEIFFileProvider _eifFileProvider;
         private readonly IEnumerable<ITradeEventNotifier> _tradeEventNotifiers;
 
         public override PacketAction Action => PacketAction.Use;
 
         public TradeUseHandler(IPlayerInfoProvider playerInfoProvider,
                                ITradeRepository tradeRepository,
-                               ICharacterProvider characterProvider,
+                               ICharacterRepository characterRepository,
                                ICharacterInventoryRepository characterInventoryRepository,
+                               IEIFFileProvider eifFileProvider,
                                IEnumerable<ITradeEventNotifier> tradeEventNotifiers)
             : base(playerInfoProvider, tradeRepository)
         {
-            _characterProvider = characterProvider;
+            _characterRepository = characterRepository;
             _characterInventoryRepository = characterInventoryRepository;
+            _eifFileProvider = eifFileProvider;
             _tradeEventNotifiers = tradeEventNotifiers;
         }
 
@@ -101,10 +105,11 @@ namespace EOLib.PacketHandlers.Trade
             base.HandlePacket(packet);
 
             var (removeItems, addItems) = _tradeRepository
-                .SomeWhen(x => x.PlayerOneOffer.PlayerID == _characterProvider.MainCharacter.ID)
+                .SomeWhen(x => x.PlayerOneOffer.PlayerID == _characterRepository.MainCharacter.ID)
                 .Match(some: x => (x.PlayerOneOffer.Items, x.PlayerTwoOffer.Items),
                        none: () => (_tradeRepository.PlayerTwoOffer.Items, _tradeRepository.PlayerOneOffer.Items));
 
+            var stats = _characterRepository.MainCharacter.Stats;
             foreach (var removedItem in removeItems)
             {
                 _characterInventoryRepository.ItemInventory.SingleOrNone(x => x.ItemID == removedItem.ItemID)
@@ -116,6 +121,8 @@ namespace EOLib.PacketHandlers.Trade
                             _characterInventoryRepository.ItemInventory.Add(x.WithAmount(x.Amount - removedItem.Amount));
                         }
                     });
+
+                stats = stats.WithNewStat(CharacterStat.Weight, stats[CharacterStat.Weight] - _eifFileProvider.EIFFile[removedItem.ItemID].Weight * removedItem.Amount);
             }
 
             foreach (var newItem in addItems)
@@ -130,7 +137,11 @@ namespace EOLib.PacketHandlers.Trade
                     {
                         _characterInventoryRepository.ItemInventory.Add(newItem);
                     });
+
+                stats = stats.WithNewStat(CharacterStat.Weight, stats[CharacterStat.Weight] + _eifFileProvider.EIFFile[newItem.ItemID].Weight * newItem.Amount);
             }
+
+            _characterRepository.MainCharacter = _characterRepository.MainCharacter.WithStats(stats);
 
             foreach (var notifier in _tradeEventNotifiers)
                 notifier.NotifyTradeClose(cancel: false);
