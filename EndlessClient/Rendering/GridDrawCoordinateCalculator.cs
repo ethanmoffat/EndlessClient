5b7e5d079c3a105c02698b09dc142ab9f1228b13
Ethan Moffat <ethan@moffat.io>
2022-09-29 17:17:39 -0700

    PR cleanup: move method, update comment

--- EndlessClient/Rendering/GridDrawCoordinateCalculator.cs
+++ EndlessClient/Rendering/GridDrawCoordinateCalculator.cs
@@ -68,6 +68,32 @@ namespace EndlessClient.Rendering
                                ViewportHeightFactor - (props.MapY * 16) - (props.MapX * 16)) - CalculateGroundLayerCharacterOffsets();
         }
 
+        public MapCoordinate CalculateGridCoordinatesFromDrawLocation(Vector2 drawLocation)
+        {
+            //need to solve this system of equations to get x, y on the grid
+            //(x * 32) - (y * 32) + 288 - c.OffsetX, => pixX = 32x - 32y + 288 - c.OffsetX
+            //(y * 16) + (x * 16) + 144 - c.OffsetY  => 2pixY = 32y + 32x + 288 - 2c.OffsetY
+            //                                         => 2pixY + pixX = 64x + 576 - c.OffsetX - 2c.OffsetY
+            //                                         => 2pixY + pixX - 576 + c.OffsetX + 2c.OffsetY = 64x
+            //                                         => _gridX = (pixX + 2pixY - 576 + c.OffsetX + 2c.OffsetY) / 64; <=
+            //pixY = (_gridX * 16) + (_gridY * 16) + 144 - c.OffsetY =>
+            //(pixY - (_gridX * 16) - 144 + c.OffsetY) / 16 = _gridY
+
+            const int GridSpaceWidth = 64;
+            const int GridSpaceHeight = 32;
+
+            var msX = drawLocation.X - GridSpaceWidth / 2;
+            var msY = drawLocation.Y - GridSpaceHeight / 2;
+
+            var offsetX = _renderOffsetCalculator.CalculateOffsetX(_characterProvider.MainCharacter.RenderProperties);
+            var offsetY = _renderOffsetCalculator.CalculateOffsetY(_characterProvider.MainCharacter.RenderProperties);
+
+            var gridX = (int)Math.Round((msX + 2 * msY - 576 + offsetX + 2 * offsetY) / 64.0);
+            var gridY = (int)Math.Round((msY - gridX * 16 - 144 + offsetY) / 16.0);
+
+            return new MapCoordinate(gridX, gridY);
+        }
+
         private Vector2 CalculateCharacterOffsets()
         {
             var props = _characterProvider.MainCharacter.RenderProperties;
@@ -98,32 +124,6 @@ namespace EndlessClient.Rendering
 
             return new Vector2(_renderOffsetCalculator.CalculateWalkAdjustX(props),
                                _renderOffsetCalculator.CalculateWalkAdjustY(props));
-        }
-
-        public MapCoordinate CalculateGridCoordinatesFromDrawLocation(Vector2 drawLocation)
-        {
-            //need to solve this system of equations to get x, y on the grid
-            //(x * 32) - (y * 32) + 288 - c.OffsetX, => pixX = 32x - 32y + 288 - c.OffsetX
-            //(y * 16) + (x * 16) + 144 - c.OffsetY  => 2pixY = 32y + 32x + 288 - 2c.OffsetY
-            //                                         => 2pixY + pixX = 64x + 576 - c.OffsetX - 2c.OffsetY
-            //                                         => 2pixY + pixX - 576 + c.OffsetX + 2c.OffsetY = 64x
-            //                                         => _gridX = (pixX + 2pixY - 576 + c.OffsetX + 2c.OffsetY) / 64; <=
-            //pixY = (_gridX * 16) + (_gridY * 16) + 144 - c.OffsetY =>
-            //(pixY - (_gridX * 16) - 144 + c.OffsetY) / 16 = _gridY
-
-            const int GridSpaceWidth = 64;
-            const int GridSpaceHeight = 32;
-
-            var msX = drawLocation.X - GridSpaceWidth/2;
-            var msY = drawLocation.Y - GridSpaceHeight/2;
-
-            var offsetX = _renderOffsetCalculator.CalculateOffsetX(_characterProvider.MainCharacter.RenderProperties);
-            var offsetY = _renderOffsetCalculator.CalculateOffsetY(_characterProvider.MainCharacter.RenderProperties);
-
-            var gridX = (int)Math.Round((msX + 2 * msY - 576 + offsetX + 2 * offsetY) / 64.0);
-            var gridY = (int)Math.Round((msY - gridX * 16 - 144 + offsetY) / 16.0);
-
-            return new MapCoordinate(gridX, gridY);
         }
     }
 
