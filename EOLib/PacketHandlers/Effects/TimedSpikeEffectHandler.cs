32566cbc93be23894b2a693c7767bd2fc53a0ecf
Ethan Moffat <ethan@moffat.io>
2022-12-09 03:04:05 -0800

    Only play timed spike sound effect when spikes are in range. Emulates vanilla client behavior.

--- EOLib/PacketHandlers/Effects/TimedSpikeEffectHandler.cs
+++ EOLib/PacketHandlers/Effects/TimedSpikeEffectHandler.cs
@@ -1,16 +1,22 @@
 ﻿using AutomaticTypeMapper;
+using EOLib.Domain.Character;
+using EOLib.Domain.Extensions;
 using EOLib.Domain.Login;
+using EOLib.Domain.Map;
 using EOLib.Domain.Notifiers;
 using EOLib.IO.Map;
 using EOLib.Net;
 using EOLib.Net.Handlers;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EOLib.PacketHandlers.Effects
 {
     [AutoMappedType]
     public class TimedSpikeEffectHandler : InGameOnlyPacketHandler
     {
+        private readonly ICurrentMapProvider _currentMapProvider;
+        private readonly ICharacterProvider _characterProvider;
         private readonly IEnumerable<IEffectNotifier> _effectNotifiers;
 
         public override PacketFamily Family => PacketFamily.Effect;
@@ -18,9 +24,13 @@ namespace EOLib.PacketHandlers.Effects
         public override PacketAction Action => PacketAction.Report;
 
         public TimedSpikeEffectHandler(IPlayerInfoProvider playerInfoProvider,
+                                       ICurrentMapProvider currentMapProvider,
+                                       ICharacterProvider characterProvider,
                                        IEnumerable<IEffectNotifier> effectNotifiers)
             : base(playerInfoProvider)
         {
+            _currentMapProvider = currentMapProvider;
+            _characterProvider = characterProvider;
             _effectNotifiers = effectNotifiers;
         }
 
@@ -29,8 +39,14 @@ namespace EOLib.PacketHandlers.Effects
             if ((char)packet.ReadByte() != 'S')
                 return false;
 
-            foreach (var notifier in _effectNotifiers)
-                notifier.NotifyMapEffect(MapEffect.Spikes);
+            var characterPosition = _characterProvider.MainCharacter.RenderProperties.Coordinates();
+            var distanceToSpikes = _currentMapProvider.CurrentMap.GetDistanceToClosestTileSpec(TileSpec.SpikesTimed, characterPosition);
+
+            if (distanceToSpikes <= 6)
+            {
+                foreach (var notifier in _effectNotifiers)
+                    notifier.NotifyMapEffect(MapEffect.Spikes);
+            }
 
             return true;
         }
