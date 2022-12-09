3a24a28b51d87bf6e628f677375a77a589f6c9bf
Ethan Moffat <ethan@moffat.io>
2022-12-09 03:14:46 -0800

    Allow locked doors to be opened by other players

--- EOLib/Domain/Map/UnlockDoorValidator.cs
+++ EOLib/Domain/Map/UnlockDoorValidator.cs
@@ -11,19 +11,23 @@ namespace EOLib.Domain.Map
     public class UnlockDoorValidator : IUnlockDoorValidator
     {
         private readonly ICharacterInventoryProvider _characterInventoryProvider;
+        private readonly ICurrentMapStateProvider _currentMapStateProvider;
         private readonly IEIFFileProvider _eifFileProvider;
 
         public UnlockDoorValidator(ICharacterInventoryProvider characterInventoryProvider,
-                              IEIFFileProvider eifFileProvider)
+                                   ICurrentMapStateProvider currentMapStateProvider,
+                                   IEIFFileProvider eifFileProvider)
         {
             _characterInventoryProvider = characterInventoryProvider;
+            _currentMapStateProvider = currentMapStateProvider;
             _eifFileProvider = eifFileProvider;
         }
 
         public bool CanMainCharacterOpenDoor(Warp warp)
         {
-            return (int)warp.DoorType <= 1 || _characterInventoryProvider.ItemInventory
-                .Any(x => _eifFileProvider.EIFFile[x.ItemID].Type == IO.ItemType.Key && _eifFileProvider.EIFFile[x.ItemID].Key == (int)warp.DoorType);
+            return (int)warp.DoorType <= 1 || _currentMapStateProvider.OpenDoors.Any(d => d.X == warp.X && d.Y == warp.Y) ||
+                _characterInventoryProvider.ItemInventory.Any(x => _eifFileProvider.EIFFile[x.ItemID].Type == IO.ItemType.Key &&
+                                                                   _eifFileProvider.EIFFile[x.ItemID].Key == (int)warp.DoorType);
         }
 
         public Option<string> GetRequiredKey(Warp warp)
