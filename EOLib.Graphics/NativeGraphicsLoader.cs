5f3389f85fd40f00a7d2be9a5c205de9ac1618a6
dan oak <dan@oaks.family>
2022-12-12 20:10:28 +0000

    remove additional precompilation flags

--- EOLib.Graphics/NativeGraphicsLoader.cs
+++ EOLib.Graphics/NativeGraphicsLoader.cs
@@ -23,14 +23,14 @@ namespace EOLib.Graphics
             }
             catch (ArgumentException)
             {
-#if DEBUG && THROW_ON_TEXTURE_FAILURE
+#if DEBUG
                 throw;
 #endif
             }
 
             if (fileBytes.Length == 0)
             {
-#if DEBUG && THROW_ON_TEXTURE_FAILURE
+#if DEBUG
                 throw new GFXLoadException(resourceValue, file);
 #else
                 return new Image<SixLabors.ImageSharp.PixelFormats.Rgba32>(1, 1);
