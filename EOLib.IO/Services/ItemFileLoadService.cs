e67757cf2f4b4b92b316454c0052daf2e2959bb7
Ethan Moffat <ethan@moffat.io>
2023-04-24 15:37:27 -0700

    Consolidate code changes in pub load services

--- EOLib.IO/Services/ItemFileLoadService.cs
+++ EOLib.IO/Services/ItemFileLoadService.cs
@@ -1,41 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using AutomaticTypeMapper;
+﻿using AutomaticTypeMapper;
 using EOLib.IO.Pub;
 using EOLib.IO.Services.Serializers;
 
 namespace EOLib.IO.Services
 {
     [AutoMappedType]
-    public class ItemFileLoadService : IPubLoadService<EIFRecord>
+    public class ItemFileLoadService : BasePubLoadService<EIFRecord>
     {
-        private readonly IPubFileDeserializer _pubFileDeserializer;
+        protected override string FileFilter => PubFileNameConstants.EIFFilter;
 
         public ItemFileLoadService(IPubFileDeserializer pubFileDeserializer)
+            : base(pubFileDeserializer)
         {
-            _pubFileDeserializer = pubFileDeserializer;
         }
 
-        public IEnumerable<IPubFile<EIFRecord>> LoadPubFromDefaultFile()
-        {
-            return LoadPubFromExplicitFile(PubFileNameConstants.PubDirectory, PubFileNameConstants.EIFFilter);
-        }
-
-        public IEnumerable<IPubFile<EIFRecord>> LoadPubFromExplicitFile(string directory, string searchPattern)
-        {
-            var files = Directory.GetFiles(directory, searchPattern, SearchOption.TopDirectoryOnly);
-            if (files.Length == 0)
-                throw new ArgumentException($"No pub files matching {searchPattern} were found in {directory}", nameof(searchPattern));
-
-            Array.Sort(files);
-
-            int fileId = 1;
-            foreach (var file in files)
-            {
-                var fileBytes = File.ReadAllBytes(file);
-                yield return _pubFileDeserializer.DeserializeFromByteArray(fileId++, fileBytes, () => new EIFFile());
-            }
-        }
+        protected override IPubFile<EIFRecord> Factory() => new EIFFile();
     }
 }
--- EOLib.IO/Services/SpellFileLoadService.cs
+++ EOLib.IO/Services/SpellFileLoadService.cs
@@ -1,41 +1,19 @@
-﻿using System.Collections.Generic;
-using System;
-using System.IO;
-using AutomaticTypeMapper;
+﻿using AutomaticTypeMapper;
 using EOLib.IO.Pub;
 using EOLib.IO.Services.Serializers;
 
 namespace EOLib.IO.Services
 {
     [AutoMappedType]
-    public class SpellFileLoadService : IPubLoadService<ESFRecord>
+    public class SpellFileLoadService : BasePubLoadService<ESFRecord>
     {
-        private readonly IPubFileDeserializer _pubFileDeserializer;
+        protected override string FileFilter => PubFileNameConstants.ESFFilter;
 
         public SpellFileLoadService(IPubFileDeserializer pubFileDeserializer)
+            : base(pubFileDeserializer)
         {
-            _pubFileDeserializer = pubFileDeserializer;
         }
 
-        public IEnumerable<IPubFile<ESFRecord>> LoadPubFromDefaultFile()
-        {
-            return LoadPubFromExplicitFile(PubFileNameConstants.PubDirectory, PubFileNameConstants.ESFFilter);
-        }
-
-        public IEnumerable<IPubFile<ESFRecord>> LoadPubFromExplicitFile(string directory, string searchPattern)
-        {
-            var files = Directory.GetFiles(directory, searchPattern, SearchOption.TopDirectoryOnly);
-            if (files.Length == 0)
-                throw new ArgumentException($"No pub files matching {searchPattern} were found in {directory}", nameof(searchPattern));
-
-            Array.Sort(files);
-
-            int fileId = 1;
-            foreach (var file in files)
-            {
-                var fileBytes = File.ReadAllBytes(file);
-                yield return _pubFileDeserializer.DeserializeFromByteArray(fileId++, fileBytes, () => new ESFFile());
-            }
-        }
+        protected override IPubFile<ESFRecord> Factory() => new ESFFile();
     }
 }
