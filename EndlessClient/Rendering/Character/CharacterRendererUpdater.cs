ethanmoffat/EndlessClient

commit 2ceeff5228840f61f97d966546f7acf8f783743f

Character/NPC animation improvements/bugfixes - Use stopwatch instead of DateTime for animation timings (more precise) - Fix glitchy character movement for main character - Fix character walking to support continuous walk when key pressed - Fix character walking so you can change direction while walking - Fix character attacking to support continuous attack when key pressed - Fix bounds checking for water splash effect to not crash

EndlessClient/Rendering/Character/CharacterRendererUpdater.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using AutomaticTypeMapper;
+﻿using AutomaticTypeMapper;
 using EndlessClient.Rendering.Factories;
 using EOLib;
 using EOLib.Domain.Character;
 using EOLib.Domain.Map;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EndlessClient.Rendering.Character
 {
@@ -131,7 +130,6 @@ namespace EndlessClient.Rendering.Character
 
         private void UpdateDeadCharacters()
         {
-            var now = DateTime.Now;
             var deadCharacters = new List<ICharacter>();
 
             foreach (var character in _currentMapStateRepository.Characters.Where(x => x.RenderProperties.IsDead))
@@ -139,9 +137,9 @@ namespace EndlessClient.Rendering.Character
                 var actionTime = _characterStateCache.DeathStartTimes.SingleOrDefault(x => x.UniqueID == character.ID);
                 if (actionTime == null)
                 {
-                    _characterStateCache.AddDeathStartTime(character.ID, DateTime.Now);
+                    _characterStateCache.AddDeathStartTime(character.ID);
                 }
-                else if ((now - actionTime.ActionStartTime).TotalSeconds > 2)
+                else if (actionTime.ActionTimer.ElapsedMilliseconds >= 2)
                 {
                     _characterStateCache.RemoveDeathStartTime(character.ID);
                     _characterStateCache.RemoveCharacterState(character.ID);

EOLib/Domain/Character/CharacterRenderProperties.cs
@@ -173,6 +173,7 @@ namespace EOLib.Domain.Character
         {
             var props = MakeCopy(this);
             props.RenderWalkFrame = 0;
+            props.ActualWalkFrame = 0;
             props.AttackFrame = 0;
             props.EmoteFrame = 0;
             props.CurrentAction = CharacterActionState.Standing;

EOLib/Domain/Character/WalkValidationActions.cs
@@ -53,7 +53,7 @@ namespace EOLib.Domain.Character
         {
             var mainCharacter = _characterProvider.MainCharacter;
 
-            if (cellState.Character.HasValue) //todo: walk through players after certain elapsed time
+            if (cellState.Character.HasValue && cellState.Character.Value != mainCharacter) //todo: walk through players after certain elapsed time
                 return mainCharacter.NoWall && IsTileSpecWalkable(cellState.TileSpec);
             if (cellState.NPC.HasValue)
                 return mainCharacter.NoWall && IsTileSpecWalkable(cellState.TileSpec);

EOLib/PacketHandlers/EndPlayerWarpHandler.cs
@@ -55,7 +55,7 @@ namespace EOLib.PacketHandlers
             var updatedMainCharacter = warpAgreePacketData.Characters.Single(MainCharacterIDMatches);
 
             //character.renderproperties.isdead is set True by the attack handler
-            //the character needs to be brought back to life when the are taken to the home map
+            //the character needs to be brought back to life when they are taken to the home map
             var bringBackToLife = _characterRepository.MainCharacter.RenderProperties.WithAlive();
             _characterRepository.MainCharacter = _characterRepository.MainCharacter
                 .WithRenderProperties(bringBackToLife)

EndlessClient/Controllers/ArrowKeyController.cs
@@ -77,7 +77,7 @@ namespace EndlessClient.Controllers
         private bool CanWalkAgain()
         {
             return _characterProvider.MainCharacter.RenderProperties.IsActing(CharacterActionState.Standing) ||
-                   _characterProvider.MainCharacter.RenderProperties.RenderWalkFrame == CharacterRenderProperties.MAX_NUMBER_OF_WALK_FRAMES;
+                   _characterProvider.MainCharacter.RenderProperties.ActualWalkFrame == CharacterRenderProperties.MAX_NUMBER_OF_WALK_FRAMES - 1;
         }
 
         private bool CurrentDirectionIs(EODirection direction)
@@ -87,14 +87,17 @@ namespace EndlessClient.Controllers
 
         private void FaceOrAttemptWalk(EODirection direction, bool faceAndMove)
         {
-            if (!CurrentDirectionIs(direction))
+            if (!CurrentDirectionIs(direction) && _characterProvider.MainCharacter.RenderProperties.IsActing(CharacterActionState.Standing))
             {
                 FaceDirection(direction);
                 if (faceAndMove)
                     AttemptToStartWalking();
             }
             else
+            {
+                _characterAnimationActions.Face(direction);
                 AttemptToStartWalking();
+            }
         }
 
         private void FaceDirection(EODirection direction)
@@ -112,7 +115,6 @@ namespace EndlessClient.Controllers
             else
             {
                 _characterAnimationActions.StartWalking();
-                _characterActions.Walk();
 
                 var coordinate = new MapCoordinate(
                     _characterProvider.MainCharacter.RenderProperties.MapX,

EndlessClient/Controllers/ControlKeyController.cs
@@ -41,10 +41,10 @@ namespace EndlessClient.Controllers
 
         private bool CanAttackAgain()
         {
-            return !_characterAnimationActions.IsAttacking(_characterProvider.MainCharacter.ID) &&
-                   (_characterProvider.MainCharacter.RenderProperties.IsActing(CharacterActionState.Standing) ||
-                   _characterProvider.MainCharacter.RenderProperties.AttackFrame == CharacterRenderProperties.MAX_NUMBER_OF_ATTACK_FRAMES ||
-                   _characterProvider.MainCharacter.RenderProperties.IsRangedWeapon && _characterProvider.MainCharacter.RenderProperties.AttackFrame == CharacterRenderProperties.MAX_NUMBER_OF_RANGED_ATTACK_FRAMES);
+            var rp = _characterProvider.MainCharacter.RenderProperties;
+            return !_characterAnimationActions.IsAttacking(_characterProvider.MainCharacter.ID) ||
+                   (!rp.IsRangedWeapon && rp.AttackFrame == CharacterRenderProperties.MAX_NUMBER_OF_ATTACK_FRAMES - 1) ||
+                   (rp.IsRangedWeapon && rp.AttackFrame == CharacterRenderProperties.MAX_NUMBER_OF_RANGED_ATTACK_FRAMES - 1);
         }
 
         private void AttemptAttack()

EndlessClient/HUD/Controls/HudControlsFactory.cs
@@ -50,7 +50,7 @@ namespace EndlessClient.HUD.Controls
         private readonly IExperienceTableProvider _experienceTableProvider;
         private readonly IArrowKeyController _arrowKeyController;
         private readonly IPathFinder _pathFinder;
-
+        private readonly ICharacterActions _characterActions;
         private IChatController _chatController;
 
         public HudControlsFactory(IHudButtonController hudButtonController,
@@ -72,7 +72,8 @@ namespace EndlessClient.HUD.Controls
                                   IChatModeCalculator chatModeCalculator,
                                   IExperienceTableProvider experienceTableProvider,
                                   IArrowKeyController arrowKeyController,
-                                  IPathFinder pathFinder)
+                                  IPathFinder pathFinder,
+                                  ICharacterActions characterActions)
         {
             _hudButtonController = hudButtonController;
             _hudPanelFactory = hudPanelFactory;
@@ -94,6 +95,7 @@ namespace EndlessClient.HUD.Controls
             _experienceTableProvider = experienceTableProvider;
             _arrowKeyController = arrowKeyController;
             _pathFinder = pathFinder;
+            _characterActions = characterActions;
         }
 
         public void InjectChatController(IChatController chatController)
@@ -336,7 +338,7 @@ namespace EndlessClient.HUD.Controls
 
         private ICharacterAnimator CreateCharacterAnimator()
         {
-            return new CharacterAnimator(_endlessGameProvider, _characterRepository, _currentMapStateRepository, _currentMapProvider);
+            return new CharacterAnimator(_endlessGameProvider, _characterRepository, _currentMapStateRepository, _currentMapProvider, _characterActions);
         }
 
         private INPCAnimator CreateNPCAnimator()

EndlessClient/Rendering/Character/CharacterAnimationActions.cs
@@ -45,11 +45,10 @@ namespace EndlessClient.Rendering.Character
 
         public void Face(EODirection direction)
         {
-            var renderProperties = _characterRepository.MainCharacter.RenderProperties;
-            renderProperties = renderProperties.WithDirection(direction);
+            if (!_hudControlProvider.IsInGame)
+                return;
 
-            var newMainCharacter = _characterRepository.MainCharacter.WithRenderProperties(renderProperties);
-            _characterRepository.MainCharacter = newMainCharacter;
+            Animator.MainCharacterFace(direction);
         }
 
         public void StartWalking()

commit 57e1ca735b27c625c57507039beb564e01d8d140

Fix death animation happening too quickly
Bug was introduced in commit 2ceeff52

EndlessClient/Rendering/Character/CharacterRendererUpdater.cs
@@ -155,7 +155,7 @@ namespace EndlessClient.Rendering.Character
                         none: () => _characterStateCache.AddDeathStartTime(character.ID),
                         some: actionTime =>
                         {
-                            if ((_fixedTimeStepRepository.TickCount - actionTime.ActionTick) >= 1) // prior value was 2ms; ticks are 12ms
+                            if ((_fixedTimeStepRepository.TickCount - actionTime.ActionTick) >= 200) // 200 ticks * 10ms = 2 seconds
                             {
                                 _characterStateCache.RemoveDeathStartTime(character.ID);
                                 _characterStateCache.RemoveCharacterState(character.ID);