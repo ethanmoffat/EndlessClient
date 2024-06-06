c7b70d5ba54996c6f2ed4750b44c3dd15a8987c5
Richard Leek <richard@richardleek.com>
2024-06-06 16:47:44 +0200

    Make group chat handler use party members instead of map data

--- EOLib/PacketHandlers/Chat/GroupChatHandler.cs
+++ EOLib/PacketHandlers/Chat/GroupChatHandler.cs
@@ -1,55 +1,60 @@
 ﻿using System.Collections.Generic;
 using AutomaticTypeMapper;
-using EOLib.Domain.Character;
 using EOLib.Domain.Chat;
 using EOLib.Domain.Login;
-using EOLib.Domain.Map;
 using EOLib.Domain.Notifiers;
+using EOLib.Domain.Party;
+using EOLib.Net.Handlers;
 using Moffat.EndlessOnline.SDK.Protocol.Net;
 using Moffat.EndlessOnline.SDK.Protocol.Net.Server;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EOLib.PacketHandlers.Chat
 {
     [AutoMappedType]
-    public class GroupChatHandler : PlayerChatByIDHandler<TalkOpenServerPacket>
+    public class GroupChatHandler : InGameOnlyPacketHandler<TalkOpenServerPacket>
     {
         private readonly IChatRepository _chatRepository;
+        private readonly IPartyDataProvider _partyDataProvider;
         private readonly IEnumerable<IOtherCharacterEventNotifier> _otherCharacterEventNotifiers;
         private readonly IEnumerable<IChatEventNotifier> _chatEventNotifiers;
 
+        public override PacketFamily Family => PacketFamily.Talk;
         public override PacketAction Action => PacketAction.Open;
 
         public GroupChatHandler(IPlayerInfoProvider playerInfoProvider,
-                                ICurrentMapStateRepository currentMapStateRepository,
-                                ICharacterProvider characterProvider,
                                 IChatRepository chatRepository,
+                                IPartyDataProvider partyDataProvider,
                                 IEnumerable<IOtherCharacterEventNotifier> otherCharacterEventNotifiers,
                                 IEnumerable<IChatEventNotifier> chatEventNotifiers)
-            : base(playerInfoProvider, currentMapStateRepository, characterProvider)
+            : base(playerInfoProvider)
         {
             _chatRepository = chatRepository;
+            _partyDataProvider = partyDataProvider;
             _otherCharacterEventNotifiers = otherCharacterEventNotifiers;
             _chatEventNotifiers = chatEventNotifiers;
         }
 
         public override bool HandlePacket(TalkOpenServerPacket packet)
         {
-            return Handle(packet, packet.PlayerId);
-        }
+            var member = _partyDataProvider.Members.FirstOrDefault(member => member.CharacterID == packet.PlayerId);
+            if (member == null)
+                return true;
 
-        protected override void DoTalk(TalkOpenServerPacket packet, Character character)
-        {
-            var localChatData = new ChatData(ChatTab.Local, character.Name, packet.Message, ChatIcon.PlayerPartyDark, ChatColor.PM);
+            var localChatData = new ChatData(ChatTab.Local, member.Name, packet.Message, ChatIcon.PlayerPartyDark, ChatColor.PM);
             _chatRepository.AllChat[ChatTab.Local].Add(localChatData);
 
-            var chatData = new ChatData(ChatTab.Group, character.Name, packet.Message, ChatIcon.PlayerPartyDark);
+            var chatData = new ChatData(ChatTab.Group, member.Name, packet.Message, ChatIcon.PlayerPartyDark);
             _chatRepository.AllChat[ChatTab.Group].Add(chatData);
 
             foreach (var notifier in _otherCharacterEventNotifiers)
-                notifier.OtherCharacterSaySomethingToGroup(character.ID, packet.Message);
+                notifier.OtherCharacterSaySomethingToGroup(member.CharacterID, packet.Message);
 
             foreach (var notifier in _chatEventNotifiers)
                 notifier.NotifyChatReceived(ChatEventType.Group);
+
+            return true;
         }
     }
 }
