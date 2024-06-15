84b38bdca9e1d611765b9896001bb0f5577f65c1
Richard Leek <richard@richardleek.com>
2024-06-15 21:18:54 +0000

    Reboot handler (#362)
    Add Message_Close handler, reboot event notifier, and reboot sound effect id

---
+++ EOLib/PacketHandlers/Message/MessageCloseHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutomaticTypeMapper;
+using EOLib.Domain.Login;
+using EOLib.Domain.Notifiers;
+using EOLib.Net.Handlers;
+using Moffat.EndlessOnline.SDK.Protocol.Net;
+using Moffat.EndlessOnline.SDK.Protocol.Net.Server;
+using System.Collections.Generic;
+
+namespace EOLib.PacketHandlers.Message
+{
+    /// <summary>
+    /// Shows the server reboot message
+    /// </summary>
+    [AutoMappedType]
+    public class MessageCloseHandler : InGameOnlyPacketHandler<MessageCloseServerPacket>
+    {
+        private readonly IEnumerable<IServerRebootNotifier> _serverRebootNotifiers;
+
+        public override PacketFamily Family => PacketFamily.Message;
+
+        public override PacketAction Action => PacketAction.Close;
+
+        public MessageCloseHandler(IPlayerInfoProvider playerInfoProvider,
+                                    IEnumerable<IServerRebootNotifier> serverRebootNotifiers)
+            : base(playerInfoProvider)
+        {
+            _serverRebootNotifiers = serverRebootNotifiers;
+        }
+        
+
+        public override bool HandlePacket(MessageCloseServerPacket packet)
+        {
+            foreach (var notifier in _serverRebootNotifiers)
+                notifier.NotifyServerReboot();
+
+            return true;
+        }
+    }
+}
