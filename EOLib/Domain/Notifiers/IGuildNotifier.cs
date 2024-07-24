eeee5d421c540a8b453c7a6cad9b105ba15cd6c4
Richard Leek <richard@richardleek.com>
2024-07-24 07:06:10 +0200

    Implement GuildReply JoinRequest

--- EOLib/Domain/Notifiers/IGuildNotifier.cs
+++ EOLib/Domain/Notifiers/IGuildNotifier.cs
@@ -6,11 +6,13 @@ namespace EOLib.Domain.Notifiers
     public interface IGuildNotifier
     {
         void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity);
+        void NotifyRequestToJoinGuild(int playerId, string name);
     }
 
     [AutoMappedType]
     public class NoOpGuildNotifier : IGuildNotifier
     {
         public void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity) { }
+        public void NotifyRequestToJoinGuild(int playerId, string name) { }
     }
 }
--- EndlessClient/Subscribers/GuildEventSubscriber.cs
+++ EndlessClient/Subscribers/GuildEventSubscriber.cs
@@ -51,5 +51,31 @@ namespace EndlessClient.Subscribers
 
             dlg.ShowDialog();
         }
+
+        public void NotifyRequestToJoinGuild(int playerId, string name)
+        {
+            _sfxPlayer.PlaySfx(SoundEffectID.ServerMessage);
+
+            var dlg = _messageBoxFactory.CreateMessageBox(
+                $"{name}" + " " +
+                _localizedStringFinder.GetString(DialogResourceID.GUILD_REQUESTED_TO_JOIN) + " " +
+                _localizedStringFinder.GetString(EOResourceID.GUILD_YOUR_ACCOUNT_WILL_BE_CHARGED) + " " +
+                _localizedStringFinder.GetString(EOResourceID.GUILD_PLEASE_CONSIDER_CAREFULLY) + " " +
+                _localizedStringFinder.GetString(EOResourceID.GUILD_DO_YOU_ACCEPT)
+                , caption: _localizedStringFinder.GetString(DialogResourceID.GUILD_PLAYER_WANTS_TO_JOIN), whichButtons: Dialogs.EODialogButtons.OkCancel,
+                style: Dialogs.EOMessageBoxStyle.LargeDialogSmallHeader);
+
+            dlg.DialogClosing += (_, e) =>
+            {
+                if (e.Result == XNAControls.XNADialogResult.OK)
+                {
+                    var packet = new GuildUseClientPacket();
+                    packet.PlayerId = playerId;
+                    _packetSendService.SendPacket(packet);
+                }
+            };
+
+            dlg.ShowDialog();
+        }
     }
 }
