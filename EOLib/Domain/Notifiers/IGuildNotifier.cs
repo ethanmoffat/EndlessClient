f25d46edc75b51e1eb78e7e258ef2ff7db2b7624
Richard Leek <richard@richardleek.com>
2024-07-25 04:29:51 +0200

    Add GuildEventNotifer and Subscriber

--- EOLib/Domain/Notifiers/IGuildNotifier.cs
+++ EOLib/Domain/Notifiers/IGuildNotifier.cs
@@ -6,11 +6,13 @@ namespace EOLib.Domain.Notifiers
     public interface IGuildNotifier
     {
         void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity);
+        void NotifyGuildDetailsUpdated();
     }
 
     [AutoMappedType]
     public class NoOpGuildNotifier : IGuildNotifier
     {
         public void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity) { }
+        public void NotifyGuildDetailsUpdated() { }
     }
 }
