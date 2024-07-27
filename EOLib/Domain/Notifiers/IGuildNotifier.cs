59fcd7b0d43fedba353b85ef15ffb94a5f69ea5e
Richard Leek <richard@richardleek.com>
2024-07-27 12:16:04 -0400

    Guild creation request (#363)
    * Implement Guild_Request packet handler
    
    * Remove GuildCreationRequest type
    Fix formatting on CreateMessageBox
    Use member initialization syntax for packet

---
+++ EOLib/Domain/Notifiers/IGuildNotifier.cs
@@ -0,0 +1,16 @@
+﻿using AutomaticTypeMapper;
+using EOLib.Domain.Interact.Guild;
+
+namespace EOLib.Domain.Notifiers
+{
+    public interface IGuildNotifier
+    {
+        void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity);
+    }
+
+    [AutoMappedType]
+    public class NoOpGuildNotifier : IGuildNotifier
+    {
+        public void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity) { }
+    }
+}
---
+++ EOLib/PacketHandlers/Guild/GuildRequestHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutomaticTypeMapper;
+using EOLib.Domain.Login;
+using EOLib.Domain.Notifiers;
+using EOLib.Net.Handlers;
+using Moffat.EndlessOnline.SDK.Protocol.Net;
+using Moffat.EndlessOnline.SDK.Protocol.Net.Server;
+using System.Collections.Generic;
+
+namespace EOLib.PacketHandlers.Guild
+{
+    [AutoMappedType]
+
+    public class GuildRequestHandler : InGameOnlyPacketHandler<GuildRequestServerPacket>
+    {
+        private readonly IEnumerable<IGuildNotifier> _guildNotifiers;
+
+        public override PacketFamily Family => PacketFamily.Guild;
+
+        public override PacketAction Action => PacketAction.Request;
+
+        public GuildRequestHandler(IPlayerInfoProvider playerInfoProvider,
+                                 IEnumerable<IGuildNotifier> guildNotifiers)
+            : base(playerInfoProvider)
+        {
+            _guildNotifiers = guildNotifiers;
+        }
+
+        public override bool HandlePacket(GuildRequestServerPacket packet)
+        {
+            foreach(var notifier in _guildNotifiers)
+            {
+                notifier.NotifyGuildCreationRequest(packet.PlayerId, packet.GuildIdentity);
+            }
+
+            return true;
+        }
+    }
+}
