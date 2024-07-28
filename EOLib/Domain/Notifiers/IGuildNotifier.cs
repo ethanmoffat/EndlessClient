8e92ead05d309953d4ba945bed393abc77b97dc7
Richard Leek <richard@richardleek.com>
2024-07-28 17:09:30 -0400

    Guild reply join request (#364)
    * Implement GuildReply JoinRequest
    
    * remove unused import
    
    * fix duplicate resource id
    
    * Fix formatting on CreateMessageBox and cleanup packet send
    
    * mend

--- EOLib/Domain/Notifiers/IGuildNotifier.cs
+++ EOLib/Domain/Notifiers/IGuildNotifier.cs
@@ -1,16 +1,17 @@
 ﻿using AutomaticTypeMapper;
-using EOLib.Domain.Interact.Guild;
 
 namespace EOLib.Domain.Notifiers
 {
     public interface IGuildNotifier
     {
         void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity);
+        void NotifyRequestToJoinGuild(int playerId, string name);
     }
 
     [AutoMappedType]
     public class NoOpGuildNotifier : IGuildNotifier
     {
         public void NotifyGuildCreationRequest(int creatorPlayerID, string guildIdentity) { }
+        public void NotifyRequestToJoinGuild(int playerId, string name) { }
     }
 }
