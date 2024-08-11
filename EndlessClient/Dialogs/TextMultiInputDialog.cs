da28f3a93740e62adab5c15621e879b0a9fd8bbe
Stephen <s.mcguinness@derby.ac.uk>
2024-08-11 13:24:35 +0100

    Text MultiDialog
    - Updated based on feedback.

--- EndlessClient/Dialogs/TextMultiInputDialog.cs
+++ EndlessClient/Dialogs/TextMultiInputDialog.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using EndlessClient.Content;
 using EndlessClient.Dialogs.Services;
 using EndlessClient.HUD.Chat;
+using EndlessClient.UIControls;
 using EOLib;
 using EOLib.Graphics;
 using Microsoft.Xna.Framework;
@@ -21,8 +22,8 @@ namespace EndlessClient.Dialogs
             Four,
             Five,
             FiveWithScroll,
+            NineWithScroll
         }
-
         public struct InputInfo
         {
             public string Label;
@@ -35,9 +36,15 @@ namespace EndlessClient.Dialogs
 
         private readonly Vector2 _bottomOverlayDrawPosition;
         private readonly Rectangle _bottomOverlaySource;
+        private readonly Vector2 _rightOverlayDrawPosition;
+        private readonly Rectangle _rightOverlaySource;
+        private readonly ScrollBar _scrollBar;
+        private readonly XNATextBox[] _inputBoxes;
+        private XNALabel[] _inputLabels;
+        private readonly DialogSize _dialogSize;
+        private readonly INativeGraphicsManager _nativeGraphicsManager;
 
-        private readonly IXNATextBox[] _inputBoxes;
-
+        private int _previousScrollOffset = -1;
         public IReadOnlyList<string> Responses => _inputBoxes.Select(x => x.Text).ToList();
 
         public TextMultiInputDialog(INativeGraphicsManager nativeGraphicsManager,
@@ -51,6 +58,10 @@ namespace EndlessClient.Dialogs
             : base(nativeGraphicsManager, isInGame: true)
         {
             _background = GraphicsManager.TextureFromResource(GFXTypes.PostLoginUI, 57);
+            _nativeGraphicsManager = nativeGraphicsManager;
+            _dialogSize = size;
+            Vector2 okButtonPosition;
+            Vector2 cancelButtonPosition;
 
             switch (size)
             {
@@ -66,7 +77,42 @@ namespace EndlessClient.Dialogs
                     _bottomOverlayDrawPosition = new Vector2(0, 111);
                     _bottomOverlaySource = new Rectangle(0, 240, 330, 59);
 
-                    _inputBoxes = new IXNATextBox[2];
+                    _inputLabels = new XNALabel[2];
+                    _inputBoxes = new XNATextBox[2];
+
+                    okButtonPosition = new Vector2(73, 125);
+                    cancelButtonPosition = new Vector2(166, 125);
+                    break;
+
+                case DialogSize.NineWithScroll:
+
+                    if (inputInfo.Length != 9)
+                    {
+                        throw new ArgumentException("Not enough input labels were provided");
+                    }
+
+                    _backgroundSourceRectangle = new Rectangle(0, 0, 330, 210);
+
+                    SetSize(330, 210);
+                    _bottomOverlayDrawPosition = new Vector2(0, 190);
+                    _rightOverlayDrawPosition = new Vector2(270, 0);
+                    _bottomOverlaySource = new Rectangle(0, 240, 330, 59);
+                    _rightOverlaySource = new Rectangle(330, 0, 40, 190);
+                    _scrollBar = new ScrollBar(new Vector2(279, 72), new Vector2(16, 107), ScrollBarColors.LightOnDark, _nativeGraphicsManager)
+                    {
+                        LinesToRender = 5,
+                        Visible = true
+                    };
+                    _scrollBar.SetParentControl(this);
+                    _scrollBar.UpdateDimensions(9);
+                    SetScrollWheelHandler(_scrollBar);
+
+                    _inputLabels = new XNALabel[9];
+                    _inputBoxes = new XNATextBox[9];
+
+                    okButtonPosition = new Vector2(73, 195);
+                    cancelButtonPosition = new Vector2(166, 195);
+
                     break;
                 default: throw new NotImplementedException();
             }
@@ -97,15 +143,15 @@ namespace EndlessClient.Dialogs
             int yCoord = 69;
             for (int i = 0; i < inputInfo.Length; i++)
             {
-                var nextLabel = new XNALabel(Constants.FontSize10)
+                _inputLabels[i] = new XNALabel(Constants.FontSize10)
                 {
                     AutoSize = true,
                     ForeColor = ColorConstants.LightGrayDialogMessage,
                     Text = inputInfo[i].Label,
                     DrawPosition = new Vector2(24, yCoord),
                 };
-                nextLabel.Initialize();
-                nextLabel.SetParentControl(this);
+                _inputLabels[i].Initialize();
+                _inputLabels[i].SetParentControl(this);
 
                 _inputBoxes[i] = new XNATextBox(new Rectangle(126, yCoord, 168, 19), Constants.FontSize08, caretTexture: contentProvider.Textures[ContentProvider.Cursor])
                 {
@@ -120,14 +166,14 @@ namespace EndlessClient.Dialogs
             }
 
             var ok = new XNAButton(eoDialogButtonService.SmallButtonSheet,
-                new Vector2(73, 125),
+                okButtonPosition,
                 eoDialogButtonService.GetSmallDialogButtonOutSource(SmallButton.Ok),
                 eoDialogButtonService.GetSmallDialogButtonOverSource(SmallButton.Ok));
             ok.OnClick += (_, _) => Close(XNADialogResult.OK);
             ok.SetParentControl(this);
 
             var cancel = new XNAButton(eoDialogButtonService.SmallButtonSheet,
-                new Vector2(166, 125),
+                cancelButtonPosition,
                 eoDialogButtonService.GetSmallDialogButtonOutSource(SmallButton.Cancel),
                 eoDialogButtonService.GetSmallDialogButtonOverSource(SmallButton.Cancel));
             cancel.OnClick += (_, _) => Close(XNADialogResult.Cancel);
@@ -154,9 +200,53 @@ namespace EndlessClient.Dialogs
             _spriteBatch.Begin();
             _spriteBatch.Draw(_background, DrawAreaWithParentOffset, _backgroundSourceRectangle, Color.White);
             _spriteBatch.Draw(_background, DrawPositionWithParentOffset + _bottomOverlayDrawPosition, _bottomOverlaySource, Color.White);
+            _spriteBatch.Draw(_background, DrawPositionWithParentOffset + _rightOverlayDrawPosition, _rightOverlaySource, Color.White);
             _spriteBatch.End();
-
             base.OnDrawControl(gameTime);
+        }
+
+        protected override void OnUpdateControl(GameTime gameTime)
+        {
+            if (_dialogSize != DialogSize.NineWithScroll)
+            {
+                base.OnUpdateControl(gameTime);
+                return;
+            }
+
+            if (_scrollBar.ScrollOffset != _previousScrollOffset)
+            {
+                for (int i = 0; i < _inputBoxes.Length; i++)
+                {
+                    var currBox = _inputBoxes[i];
+                    var currLabel = _inputLabels[i];
+
+                    if (i < _scrollBar.ScrollOffset)
+                    {
+                        currBox.Visible = false;
+                        currLabel.Visible = false;
+                        continue;
+                    }
+
+                    if (i < _scrollBar.LinesToRender + _scrollBar.ScrollOffset)
+                    {
+                        currBox.Visible = true;
+                        currLabel.Visible = true;
+
+                        int relativeIndex = i - _scrollBar.ScrollOffset;
+                        currBox.DrawPosition = new Vector2(currBox.DrawPosition.X, 69 + relativeIndex * 24);
+                        currLabel.DrawPosition = new Vector2(currLabel.DrawPosition.X, 69 + relativeIndex * 24);
+                    }
+
+                    else
+                    {
+                        currBox.Visible = false;
+                        currLabel.Visible = false;
+                    }
+                }
+
+                _previousScrollOffset = _scrollBar.ScrollOffset;
+            }
+            base.OnUpdateControl(gameTime);
         }
     }
 }
